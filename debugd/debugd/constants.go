6f3281102998bb4b177d901daf9577b079f1e624
Malte Poll <mp@edgeless.systems>
2022-04-27 10:30:40 +0200

    debugd: Upgrade default systemd unit to incorporate CNI bin dir preparation

--- debugd/debugd/constants.go
+++ debugd/debugd/constants.go
@@ -20,6 +20,9 @@ After=network-online.target
 Type=simple
 EnvironmentFile=/etc/constellation.env
 ExecStartPre=-setenforce Permissive
+ExecStartPre=/usr/bin/mkdir -p /opt/cni/bin/
+# merge all CNI binaries in writable folder until containerd can use multiple CNI bins: https://github.com/containerd/containerd/issues/6600
+ExecStartPre=/bin/sh -c "/usr/bin/cp /usr/libexec/cni/* /opt/cni/bin/"
 ExecStart=/opt/coordinator
 [Install]
 WantedBy=multi-user.target
