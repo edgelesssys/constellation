aa0a07592b1727e49b8917f2b27a25fd122072c1
Thomas Tendyck <51411342+thomasten@users.noreply.github.com>
2022-07-26 11:49:13 +0200

    check licenses (#297)
    * AB#2222 check licenses of dependencies
    
    * AB#2222 check-licenses: use setup-go

---
+++ hack/check-licenses.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Compare licenses of Go dependencies against a whitelist.
+
+set -e -o pipefail
+
+not_allowed() {
+  echo "license not allowed for package: $line"
+  err=1
+}
+
+go mod download
+
+go-licenses csv ./... | {
+while read line; do
+
+  pkg=${line%%,*}
+  lic=${line##*,}
+
+  case $lic in
+    Apache-2.0|BSD-2-Clause|BSD-3-Clause|ISC|MIT)
+      ;;
+
+    MPL-2.0)
+      case $pkg in
+        github.com/talos-systems/talos/pkg/machinery/config/encoder)
+          ;;
+        *)
+          not_allowed
+          ;;
+      esac
+      ;;
+
+    Unknown)
+      case $pkg in
+        github.com/edgelesssys/constellation/*)
+          ;;
+        *)
+          not_allowed
+          ;;
+      esac
+      ;;
+
+    *)
+      echo "unknown license: $line"
+      err=1
+      ;;
+  esac
+
+done
+exit $err
+}
