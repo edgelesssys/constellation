4176f038df61ebe64bfdcbe50cd92a713cc32ab5
Fabian Kammel <fabian@kammel.dev>
2022-08-17 16:58:36 +0200

    Generate CLI reference also for sub-commands (#374)
    * include all subcommands
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- hack/clidocgen/main.go
+++ hack/clidocgen/main.go
@@ -21,7 +21,7 @@ func main() {
 	// Generate Markdown for all commands.
 	cmdList := &bytes.Buffer{}
 	body := &bytes.Buffer{}
-	for _, c := range rootCmd.Commands() {
+	for _, c := range allSubCommands(rootCmd) {
 		name := c.Name()
 		fmt.Fprintf(cmdList, "* [%v](#constellation-%v): %v\n", name, name, c.Short)
 		if err := doc.GenMarkdown(c, body); err != nil {
@@ -33,4 +33,13 @@ func main() {
 	cleanedBody := seeAlsoRegexp.ReplaceAll(body.Bytes(), nil)
 
 	fmt.Printf("Commands:\n\n%s\n%s", cmdList, cleanedBody)
+}
+
+func allSubCommands(cmd *cobra.Command) []*cobra.Command {
+	var all []*cobra.Command
+	for _, c := range cmd.Commands() {
+		all = append(all, c)
+		all = append(all, allSubCommands(c)...)
+	}
+	return all
 }
