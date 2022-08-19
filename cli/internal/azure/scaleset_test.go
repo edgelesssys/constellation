e841d9201b2b2dd7557ad067cc9141329c652916
Malte Poll <mp@edgeless.systems>
2022-08-19 17:23:59 +0200

    Use Azure CVMs in e2e tests

--- cli/internal/azure/scaleset_test.go
+++ cli/internal/azure/scaleset_test.go
@@ -15,7 +15,7 @@ func TestFirewallPermissions(t *testing.T) {
 		Name:                 "name",
 		NamePrefix:           "constellation-",
 		Location:             "UK South",
-		InstanceType:         "Standard_D2s_v3",
+		InstanceType:         "Standard_DC2as_v5",
 		Count:                3,
 		Username:             "constellation",
 		SubnetID:             "subnet-id",
