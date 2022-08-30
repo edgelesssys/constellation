1288dee2014962ab0df9407b2792b691872cdf7e
Fabian Kammel <fk@edgeless.systems>
2022-08-30 11:22:28 +0000

    change default image to new format of public images for next release
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- internal/config/images_enterprise.go
+++ internal/config/images_enterprise.go
@@ -3,6 +3,6 @@
 package config
 
 const (
-	DefaultImageAzure = "/subscriptions/0d202bbb-4fa7-4af8-8125-58c269a05435/resourceGroups/CONSTELLATION-IMAGES/providers/Microsoft.Compute/galleries/Constellation/images/constellation/versions/1.5.0"
-	DefaultImageGCP   = "projects/constellation-images/global/images/constellation-v1-5-0"
+	DefaultImageAzure = "/CommunityGalleries/ConstellationCVM-b3782fa0-0df7-4f2f-963e-fc7fc42663df/Images/constellation/Versions/0.0.1"
+	DefaultImageGCP   = "projects/constellation-images/global/images/constellation-v0-0-1"
 )
