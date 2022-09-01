ab2a797da9be093e82c88264e8f84034b27ac55f
Fabian Kammel <fk@edgeless.systems>
2022-09-01 10:56:51 +0000

    [no ci] add more output and grouping
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- .github/actions/constellation_create/gcp-logs.sh
+++ .github/actions/constellation_create/gcp-logs.sh
@@ -6,6 +6,8 @@ CONTROL_INSTANCES=$(gcloud compute instance-groups list-instances $1 --zone $3 -
 WORKER_INSTANCES=$(gcloud compute instance-groups list-instances $2 --zone $3 --format=json | jq -r .'[] | .instance')
 ALL_INSTANCES="$CONTROL_INSTANCES $WORKER_INSTANCES"
 
+printf "Fetching logs for %s and %s\n" $CONTROL_INSTANCES $WORKER_INSTANCES
+
 for INSTANCE in $ALL_INSTANCES; do
   SHORT_NAME=${INSTANCE##*/}
   printf "Fetching for %s\n" $SHORT_NAME
