7b00005ed6dea61cb5d43b48c82eaccacd2369f6
Leonard Cohnen <lc@edgeless.systems>
2022-09-02 20:19:20 +0200

    fix qemu initialization

--- cli/internal/helm/values.go
+++ cli/internal/helm/values.go
@@ -88,5 +88,24 @@ var qemuVals = map[string]interface{}{
 		"enabled": true,
 		"type":    "wireguard",
 	},
-	"l7Proxy": false,
+	"image": map[string]interface{}{
+		"repository": "ghcr.io/3u13r/cilium",
+		"suffix":     "",
+		"tag":        "v1.12.1-edg",
+		"digest":     "sha256:fdac430143fe719331698b76fbe66410631a21afd3405407d56db260d2d6999b",
+		"useDigest":  true,
+	},
+	"operator": map[string]interface{}{
+		"image": map[string]interface{}{
+			"repository":    "ghcr.io/3u13r/operator",
+			"suffix":        "",
+			"tag":           "v1.12.1-edg",
+			"genericDigest": "sha256:a225d8d3976fd2a05cfa0c929cd32e60283abedf6bae51db4709df19b2fb70cb",
+			"useDigest":     true,
+		},
+	},
+	"kubeProxyReplacement":                "strict",
+	"enableCiliumEndpointSlice":           true,
+	"kubeProxyReplacementHealthzBindAddr": "0.0.0.0:10256",
+	"l7Proxy":                             false,
 }
