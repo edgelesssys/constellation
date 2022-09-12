2d8f2af91b207ddb618d53696b50800bf5388ebc
Leonard Cohnen <lc@edgeless.systems>
2022-09-12 15:17:27 +0200

    prepare release v2.0.0

--- internal/versions/versions.go
+++ internal/versions/versions.go
@@ -45,13 +45,13 @@ const (
 	// These images are built in a way that they support all versions currently listed in VersionConfigs.
 	KonnectivityAgentImage   = "us.gcr.io/k8s-artifacts-prod/kas-network-proxy/proxy-agent:v0.0.32"
 	KonnectivityServerImage  = "registry.k8s.io/kas-network-proxy/proxy-server:v0.0.32"
-	JoinImage                = "ghcr.io/edgelesssys/constellation/join-service:v0.0.2-0.20220909145031-b6c06735c313"
-	AccessManagerImage       = "ghcr.io/edgelesssys/constellation/access-manager:v0.0.1"
-	KmsImage                 = "ghcr.io/edgelesssys/constellation/kmsserver:v0.0.2-0.20220909145031-b6c06735c313"
-	VerificationImage        = "ghcr.io/edgelesssys/constellation/verification-service:v0.0.2-0.20220831181049-47d4c9e30423"
+	JoinImage                = "ghcr.io/edgelesssys/constellation/join-service:v2.0.0"
+	AccessManagerImage       = "ghcr.io/edgelesssys/constellation/access-manager:v2.0.0"
+	KmsImage                 = "ghcr.io/edgelesssys/constellation/kmsserver:v2.0.0"
+	VerificationImage        = "ghcr.io/edgelesssys/constellation/verification-service:v2.0.0"
 	GcpGuestImage            = "ghcr.io/edgelesssys/gcp-guest-agent:20220713.00"
 	NodeOperatorCatalogImage = "ghcr.io/edgelesssys/constellation/node-operator-catalog"
-	NodeOperatorVersion      = "v0.0.2-0.20220902144959-2f871578b2db"
+	NodeOperatorVersion      = "v2.0.0"
 	// TODO: switch node maintenance operator catalog back to upstream quay.io/medik8s/node-maintenance-operator-catalog
 	// once https://github.com/medik8s/node-maintenance-operator/issues/49 is resolved.
 	NodeMaintenanceOperatorCatalogImage = "ghcr.io/edgelesssys/constellation/node-maintenance-operator-catalog"
