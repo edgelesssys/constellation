abc56c4b639ced7802f4268a50c6757ee833747b
katexochen <49727155+katexochen@users.noreply.github.com>
2022-09-27 09:26:06 +0200

    Remove GCP client from CLI

--- internal/state/state.go
+++ internal/state/state.go
@@ -17,21 +17,6 @@ type ConstellationState struct {
 	CloudProvider  string `json:"cloudprovider,omitempty"`
 	LoadBalancerIP string `json:"bootstrapperhost,omitempty"`
 
-	GCPWorkerInstances              cloudtypes.Instances `json:"gcpworkers,omitempty"`
-	GCPControlPlaneInstances        cloudtypes.Instances `json:"gcpcontrolplanes,omitempty"`
-	GCPWorkerInstanceGroup          string               `json:"gcpworkerinstancegroup,omitempty"`
-	GCPControlPlaneInstanceGroup    string               `json:"gcpcontrolplaneinstancegroup,omitempty"`
-	GCPWorkerInstanceTemplate       string               `json:"gcpworkerinstancetemplate,omitempty"`
-	GCPControlPlaneInstanceTemplate string               `json:"gcpcontrolplaneinstancetemplate,omitempty"`
-	GCPNetwork                      string               `json:"gcpnetwork,omitempty"`
-	GCPSubnetwork                   string               `json:"gcpsubnetwork,omitempty"`
-	GCPFirewalls                    []string             `json:"gcpfirewalls,omitempty"`
-	GCPLoadbalancerIPname           string               `json:"gcploadbalanceripid,omitempty"`
-	GCPLoadbalancers                []string             `json:"gcploadbalancers,omitempty"`
-	GCPProject                      string               `json:"gcpproject,omitempty"`
-	GCPZone                         string               `json:"gcpzone,omitempty"`
-	GCPRegion                       string               `json:"gcpregion,omitempty"`
-
 	AzureWorkerInstances       cloudtypes.Instances `json:"azureworkers,omitempty"`
 	AzureControlPlaneInstances cloudtypes.Instances `json:"azurecontrolplanes,omitempty"`
 	AzureResourceGroup         string               `json:"azureresourcegroup,omitempty"`
