96cf50a9a5c2997495e88ec31df52c195ccaafc7
Nils Hanke <nils.hanke@outlook.com>
2022-10-05 17:18:06 +0200

    Fix KVM permission issues

--- cli/internal/libvirt/start.sh
+++ cli/internal/libvirt/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Assign qemu the GID of the host system's 'kvm' group to avoid permission issues for environments defaulting to 660 for /dev/kvm (e.g. Debian-based distros)
+usermod -a -G $(stat -c '%g' /dev/kvm) qemu
+
 # Start libvirt daemon
 libvirtd --daemon --listen
 virtlogd --daemon
