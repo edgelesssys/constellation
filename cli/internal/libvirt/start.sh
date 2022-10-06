a6c52165a89d44a2945a095a815b546f467d9348
Daniel Weiße <dw@edgeless.systems>
2022-10-06 10:43:29 +0200

    quotes
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- cli/internal/libvirt/start.sh
+++ cli/internal/libvirt/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Assign qemu the GID of the host system's 'kvm' group to avoid permission issues for environments defaulting to 660 for /dev/kvm (e.g. Debian-based distros)
-usermod -a -G $(stat -c '%g' /dev/kvm) qemu
+usermod -a -G "$(stat -c '%g' /dev/kvm)" qemu
 
 # Start libvirt daemon
 libvirtd --daemon --listen
