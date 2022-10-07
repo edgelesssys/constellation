8736699145e38c9916554a0ce098a880b7cbeb14
katexochen <49727155+katexochen@users.noreply.github.com>
2022-10-07 22:27:46 +0200

    Bump enterprise image version

--- internal/config/images_enterprise.go
+++ internal/config/images_enterprise.go
@@ -9,6 +9,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 package config
 
 const (
-	DefaultImageAzure = "/CommunityGalleries/ConstellationCVM-b3782fa0-0df7-4f2f-963e-fc7fc42663df/Images/constellation/Versions/2.0.0"
-	DefaultImageGCP   = "projects/constellation-images/global/images/constellation-v2-0-0"
+	DefaultImageAzure = "/CommunityGalleries/ConstellationCVM-b3782fa0-0df7-4f2f-963e-fc7fc42663df/Images/constellation/Versions/2.1.0"
+	DefaultImageGCP   = "projects/constellation-images/global/images/constellation-v2-1-0"
 )
