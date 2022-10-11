80c1365ec3fcdb5247135bb031bcfcdebe8e742f
Daniel Weiße <dw@edgeless.systems>
2022-10-11 16:26:17 +0200

    Implement AWS cloud logging
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- go.mod
+++ go.mod
@@ -54,6 +54,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.17.8
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17
 	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.11.34
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.15.20
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.32.0
 	github.com/aws/aws-sdk-go-v2/service/kms v1.18.12
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11
---
+++ internal/cloud/aws/logger_test.go
@@ -0,0 +1,297 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package aws
+
+import (
+	"context"
+	"errors"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	logs "github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateStream(t *testing.T) {
+	someErr := errors.New("failed")
+
+	testCases := map[string]struct {
+		imds       *stubIMDS
+		logs       *stubLogs
+		wantGroup  string
+		wantStream string
+		wantErr    bool
+	}{
+		"success new stream minimal": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+					tagUID:  "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{LogGroupName: aws.String("test-group")},
+					},
+				},
+				listTags: map[string]map[string]string{"test-group": {tagUID: "uid"}},
+			},
+			wantStream: "test-instance",
+			wantGroup:  "test-group",
+		},
+		"success one group of many": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+					tagUID:  "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{
+							LogGroupName: aws.String("random-group"),
+						},
+						{
+							LogGroupName: aws.String("other-group"),
+						},
+					},
+					NextToken: aws.String("next"),
+				},
+				describeRes2: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{
+							LogGroupName: aws.String("another-group"),
+						},
+						{
+							LogGroupName: aws.String("test-group"),
+						},
+					},
+				},
+				listTags: map[string]map[string]string{
+					"random-group": {
+						"some-tag": "random-tag",
+					},
+					"other-group": {
+						tagUID: "other-uid",
+					},
+					"another-group": {
+						"some-tag": "uid",
+					},
+					"test-group": {
+						tagUID: "uid",
+					},
+				},
+			},
+			wantStream: "test-instance",
+			wantGroup:  "test-group",
+		},
+		"success stream exists": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+					tagUID:  "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{LogGroupName: aws.String("test-group")},
+					},
+				},
+				listTags:  map[string]map[string]string{"test-group": {tagUID: "uid"}},
+				createErr: &types.ResourceAlreadyExistsException{},
+			},
+			wantStream: "test-instance",
+			wantGroup:  "test-group",
+		},
+		"create stream error": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+					tagUID:  "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{LogGroupName: aws.String("test-group")},
+					},
+				},
+				listTags:  map[string]map[string]string{"test-group": {tagUID: "uid"}},
+				createErr: someErr,
+			},
+			wantErr: true,
+		},
+		"missing uid tag": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{LogGroupName: aws.String("test-group")},
+					},
+				},
+				listTags: map[string]map[string]string{"test-group": {tagUID: "uid"}},
+			},
+			wantErr: true,
+		},
+		"missing name tag": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagUID: "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{
+					LogGroups: []types.LogGroup{
+						{LogGroupName: aws.String("test-group")},
+					},
+				},
+				listTags: map[string]map[string]string{"test-group": {tagUID: "uid"}},
+			},
+			wantErr: true,
+		},
+		"describe groups error": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+					tagUID:  "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeErr: someErr,
+				listTags:    map[string]map[string]string{"test-group": {tagUID: "uid"}},
+			},
+			wantErr: true,
+		},
+		"no matching groups": {
+			imds: &stubIMDS{
+				tags: map[string]string{
+					tagName: "test-instance",
+					tagUID:  "uid",
+				},
+			},
+			logs: &stubLogs{
+				describeRes1: &logs.DescribeLogGroupsOutput{},
+				listTags:     map[string]map[string]string{"test-group": {tagUID: "uid"}},
+			},
+			wantErr: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			l := &Logger{
+				api: tc.logs,
+			}
+
+			err := l.createStream(context.Background(), tc.imds)
+			if tc.wantErr {
+				assert.Error(err)
+				return
+			}
+
+			assert.NoError(err)
+			assert.Equal(tc.wantGroup, l.groupName)
+			assert.Equal(tc.wantStream, l.streamName)
+		})
+	}
+}
+
+func TestLogging(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	logAPI := &stubLogs{}
+
+	l := &Logger{
+		api:      logAPI,
+		interval: 1 * time.Millisecond,
+	}
+
+	l.Disclose("msg")
+	l.Disclose("msg")
+	// no logs until we flush to the API
+	assert.Len(logAPI.logs, 0)
+
+	// flush
+	require.NoError(l.flushLogs())
+	assert.Len(logAPI.logs, 2)
+
+	// flushing doesn't do anything if there are no logs
+	require.NoError(l.flushLogs())
+	assert.Len(logAPI.logs, 2)
+
+	// if we flush with an incorrect sequence token,
+	// we should get a new sequence token and retry
+	logAPI.logSequenceToken = 15
+	l.Disclose("msg")
+	require.NoError(l.flushLogs())
+	assert.Len(logAPI.logs, 3)
+
+	logAPI.putErr = errors.New("failed")
+	l.Disclose("msg")
+	assert.Error(l.flushLogs())
+}
+
+type stubLogs struct {
+	createErr        error
+	describeErr      error
+	describeRes1     *logs.DescribeLogGroupsOutput
+	describeRes2     *logs.DescribeLogGroupsOutput
+	listTagsErr      error
+	listTags         map[string]map[string]string
+	putErr           error
+	logSequenceToken int
+	logs             []types.InputLogEvent
+}
+
+func (s *stubLogs) CreateLogStream(context.Context, *logs.CreateLogStreamInput, ...func(*logs.Options)) (*logs.CreateLogStreamOutput, error) {
+	return nil, s.createErr
+}
+
+func (s *stubLogs) DescribeLogGroups(_ context.Context, in *logs.DescribeLogGroupsInput, _ ...func(*logs.Options)) (*logs.DescribeLogGroupsOutput, error) {
+	if in.NextToken == nil {
+		return s.describeRes1, s.describeErr
+	}
+	return s.describeRes2, s.describeErr
+}
+
+func (s *stubLogs) ListTagsLogGroup(_ context.Context, in *logs.ListTagsLogGroupInput, _ ...func(*logs.Options)) (*logs.ListTagsLogGroupOutput, error) {
+	return &logs.ListTagsLogGroupOutput{Tags: s.listTags[*in.LogGroupName]}, s.listTagsErr
+}
+
+func (s *stubLogs) PutLogEvents(_ context.Context, in *logs.PutLogEventsInput, _ ...func(*logs.Options)) (*logs.PutLogEventsOutput, error) {
+	if s.putErr != nil {
+		return nil, s.putErr
+	}
+	if in.SequenceToken == nil || *in.SequenceToken == "" {
+		in.SequenceToken = aws.String("0")
+	}
+	gotSeq, err := strconv.Atoi(*in.SequenceToken)
+	if err != nil {
+		return nil, err
+	}
+	if gotSeq != s.logSequenceToken {
+		return nil, &types.InvalidSequenceTokenException{ExpectedSequenceToken: aws.String(strconv.Itoa(s.logSequenceToken))}
+	}
+
+	s.logs = append(s.logs, in.LogEvents...)
+	s.logSequenceToken++
+
+	return &logs.PutLogEventsOutput{NextSequenceToken: aws.String(strconv.Itoa(s.logSequenceToken))}, nil
+}
