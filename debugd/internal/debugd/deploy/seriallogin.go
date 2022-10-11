39d51127bcb2dcdfccb4caec7de2c89da884b0db
Malte Poll <mp@edgeless.systems>
2022-10-11 17:03:49 +0200

    debugd: Allow autologin on serial console

--- debugd/internal/debugd/deploy/seriallogin.go
+++ debugd/internal/debugd/deploy/seriallogin.go
@@ -9,16 +9,33 @@ package deploy
 import (
 	"context"
 	"fmt"
-	"os/exec"
+	"os"
+	"path"
+
+	"github.com/edgelesssys/constellation/v2/debugd/internal/debugd"
+	"github.com/spf13/afero"
 )
 
-// DeleteUserPassword sets the user's password to an empty string
-// effectively allowing anyone with access to the serial console to log in.
-func DeleteUserPassword(ctx context.Context, user string) error {
-	cmd := exec.CommandContext(ctx, "passwd", "-d", user)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("deleting user password: %q %w", output, err)
+// EnableAutoLogin installs a systemd unit override that allows passwordless root login
+// on the serial console.
+func EnableAutoLogin(ctx context.Context, fs afero.Fs, serviceManager serviceManager) error {
+	if err := fs.MkdirAll(path.Dir(debugd.GettyAutologinOverrideFilename), os.ModePerm); err != nil {
+		return fmt.Errorf("creating getty autologin override directory: %w", err)
+	}
+	if err := afero.WriteFile(fs, debugd.GettyAutologinOverrideFilename,
+		[]byte(debugd.GettyAutologinOverrideUnitContents), os.ModePerm); err != nil {
+		return fmt.Errorf("writing getty autologin override unit: %w", err)
+	}
+	if err := serviceManager.SystemdAction(ctx, ServiceManagerRequest{
+		Action: Reload,
+	}); err != nil {
+		return fmt.Errorf("reloading systemd units: %w", err)
+	}
+	if err := serviceManager.SystemdAction(ctx, ServiceManagerRequest{
+		Action: Restart,
+		Unit:   "serial-getty@ttyS0.service",
+	}); err != nil {
+		return fmt.Errorf("restarting getty: %w", err)
 	}
 	return nil
 }
