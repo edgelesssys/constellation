b8f384576936accab681524157bf3584fcbc36e6
Nils Hanke <nils.hanke@outlook.com>
2022-10-13 10:29:09 +0200

    Remove default values from AWS Terraform variables.tf

--- cli/internal/terraform/terraform/aws/variables.tf
+++ cli/internal/terraform/terraform/aws/variables.tf
@@ -16,53 +16,44 @@ variable "iam_instance_profile_control_plane" {
 variable "instance_type" {
   type        = string
   description = "Instance type for worker nodes"
-  default     = "t3.micro"
 }
 
 variable "state_disk_type" {
   type        = string
   description = "EBS disk type for the state disk of the nodes"
-  default     = "gp2"
 }
 
 variable "state_disk_size" {
   type        = number
   description = "Disk size for the state disk of the nodes [GB]"
-  default     = 30
 }
 
 variable "control_plane_count" {
   type        = number
   description = "Number of control plane nodes"
-  default     = 1
 }
 
 variable "worker_count" {
   type        = number
   description = "Number of worker nodes"
-  default     = 1
 }
 
 variable "ami" {
   type        = string
   description = "AMI ID"
-  default     = "ami-02f3416038bdb17fb" // Ubuntu 22.04 LTS
 }
 
 variable "region" {
   type        = string
   description = "The AWS region to create the cluster in"
-  default     = "us-east-2"
 }
 
 variable "zone" {
   type        = string
   description = "The AWS availability zone name to create the cluster in"
-  default     = "us-east-2a"
 }
 
 variable "debug" {
   type        = bool
   description = "Enable debug mode. This opens up a debugd port that can be used to deploy a custom bootstrapper."
-  default     = false
 }
