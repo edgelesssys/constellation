041ef24750482876797bbbf8498b6fc898c9e8fe
Daniel Weiße <dw@edgeless.systems>
2022-10-24 12:38:34 +0200

    Dont clean up workspace if rollback fails
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- cli/internal/cloudcmd/rollback.go
+++ cli/internal/cloudcmd/rollback.go
@@ -41,7 +41,9 @@ type rollbackerTerraform struct {
 func (r *rollbackerTerraform) rollback(ctx context.Context) error {
 	var err error
 	err = multierr.Append(err, r.client.DestroyCluster(ctx))
-	err = multierr.Append(err, r.client.CleanUpWorkspace())
+	if err == nil {
+		err = multierr.Append(err, r.client.CleanUpWorkspace())
+	}
 	return err
 }
 
@@ -54,6 +56,8 @@ func (r *rollbackerQEMU) rollback(ctx context.Context) error {
 	var err error
 	err = multierr.Append(err, r.client.DestroyCluster(ctx))
 	err = multierr.Append(err, r.libvirt.Stop(ctx))
-	err = multierr.Append(err, r.client.CleanUpWorkspace())
+	if err == nil {
+		err = r.client.CleanUpWorkspace()
+	}
 	return err
 }
