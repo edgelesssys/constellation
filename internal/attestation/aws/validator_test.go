deb6cbb727033de35ba5b4353cda43295c45c262
leongross <leon.gross@rub.de>
2022-10-24 10:45:15 +0200

    update tests for validator

--- internal/attestation/aws/validator_test.go
+++ internal/attestation/aws/validator_test.go
@@ -7,11 +7,12 @@ SPDX-License-Identifier: AGPL-3.0-only
 package aws
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
-	"github.com/aws/smithy-go/middleware"
 	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/edgelesssys/constellation/v2/internal/attestation/vtpm"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -50,38 +51,42 @@ func TestGeTrustedKey(t *testing.T) {
 }
 
 func TestTpmEnabled(t *testing.T) {
-	mockDocBase := imds.GetInstanceIdentityDocumentOutput{
-		InstanceIdentityDocument: imds.InstanceIdentityDocument{
-			DevpayProductCodes:      []string{"devpayProductCodes"},
-			MarketplaceProductCodes: []string{"marketplaceProductCodes"},
-			AvailabilityZone:        "availabilityZone",
-			PrivateIP:               "privateIp",
-			Version:                 "version",
-			Region:                  "region",
-			InstanceID:              "instanceId",
-			BillingProducts:         []string{"billingProducts"},
-			InstanceType:            "instanceType",
-			AccountID:               "accountId",
-			PendingTime:             time.Now(),
-			ImageID:                 "imageId",
-			KernelID:                "kernelId",
-			RamdiskID:               "ramdiskId",
-			Architecture:            "architecture",
-		},
-		ResultMetadata: middleware.Metadata{},
+	mockDocBase := imds.InstanceIdentityDocument{
+		DevpayProductCodes:      []string{"devpayProductCodes"},
+		MarketplaceProductCodes: []string{"marketplaceProductCodes"},
+		AvailabilityZone:        "availabilityZone",
+		PrivateIP:               "privateIp",
+		Version:                 "version",
+		Region:                  "region",
+		InstanceID:              "instanceId",
+		BillingProducts:         []string{"billingProducts"},
+		InstanceType:            "instanceType",
+		AccountID:               "accountId",
+		PendingTime:             time.Now(),
+		ImageID:                 "imageId",
+		KernelID:                "kernelId",
+		RamdiskID:               "ramdiskId",
+		Architecture:            "architecture",
 	}
 
-	// is this a good idea? The images might not last forever -> also mock this
-	mockDocTpm := mockDocBase
-	mockDocTpm.ImageID = "ami-0a6cfb4290afa8e26"
-	mockDocTpm.AvailabilityZone = "us-east-2"
+	// The pinned images are valid at the time of this commit.
+	// When we finally suppoer AWS, we should make these values part of the version bump.
+	// Make sure, that the image is visable to all required users/clients. Verify using
+	// aws ec2 describe-image-attribute  --region <REGION> --image-id <IMAGE_ID> --attribute tpmSupport
 
+	// Since Amazon does not offer Linux stock AMI's with TPM support enabled, we use Windows images in this test.
+	// https://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/enable-nitrotpm-prerequisites.html
+	mockDocTpm := mockDocBase
+	mockDocTpm.ImageID = "ami-0e6b1588acc5a055f"
+	mockDocTpm.Region = "us-east-2"
+
+	// Stock aws linux (per default no TPM enabled), here could be the ami of ANY stock linux.
 	mockDocNoTpm := mockDocBase
-	mockDocNoTpm.ImageID = "ami-0a6cfb4290afa8e26"
-	mockDocNoTpm.AvailabilityZone = "us-east-2"
+	mockDocNoTpm.ImageID = "ami-0b59bfac6be064b78"
+	mockDocTpm.Region = "us-east-2"
 
 	testCases := map[string]struct {
-		idDoc   imds.GetInstanceIdentityDocumentOutput
+		idDoc   imds.InstanceIdentityDocument
 		wantErr bool
 	}{
 		"ami with tpm": {
@@ -104,7 +109,67 @@ func TestTpmEnabled(t *testing.T) {
 			} else {
 				assert.Nil(err)
 			}
+		})
+	}
+}
 
+func TestValidateCVM(t *testing.T) {
+	mockDocBase := imds.InstanceIdentityDocument{
+		DevpayProductCodes:      []string{"devpayProductCodes"},
+		MarketplaceProductCodes: []string{"marketplaceProductCodes"},
+		AvailabilityZone:        "availabilityZone",
+		PrivateIP:               "privateIp",
+		Version:                 "version",
+		Region:                  "region",
+		InstanceID:              "instanceId",
+		BillingProducts:         []string{"billingProducts"},
+		InstanceType:            "instanceType",
+		AccountID:               "accountId",
+		PendingTime:             time.Now(),
+		ImageID:                 "imageId",
+		KernelID:                "kernelId",
+		RamdiskID:               "ramdiskId",
+		Architecture:            "architecture",
+	}
+
+	mockDocTpm := mockDocBase
+	mockDocTpm.ImageID = "ami-0e6b1588acc5a055f"
+	mockDocTpm.Region = "us-east-2"
+
+	mockDocNoTpm := mockDocBase
+	mockDocNoTpm.ImageID = "ami-0b59bfac6be064b78"
+	mockDocTpm.Region = "us-east-2"
+
+	attDocTpm := vtpm.AttestationDocument{}
+	attDocTpm.UserData, _ = json.Marshal(mockDocTpm)
+
+	attDocNoTpm := vtpm.AttestationDocument{}
+	attDocNoTpm.UserData, _ = json.Marshal(mockDocNoTpm)
+
+	testCases := map[string]struct {
+		attestationDoc vtpm.AttestationDocument
+		wantErr        bool
+	}{
+		"CVM enabled": {
+			attestationDoc: attDocTpm,
+			wantErr:        false,
+		},
+		"CVM disabled": {
+			attestationDoc: attDocNoTpm,
+			wantErr:        true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			err := validateCVM(tc.attestationDoc)
+			if tc.wantErr {
+				assert.Error(err)
+			} else {
+				assert.Nil(err)
+			}
 		})
 	}
 }
