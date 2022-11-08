a23a56f6d80232e9beaf8353584e7a9a1f32a1d8
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-11-08 16:43:17 +0100

    e2e: add AWS test
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- .github/actions/constellation_create/az-logs.sh
+++ .github/actions/constellation_create/az-logs.sh
@@ -16,6 +16,6 @@ for scaleset in $scalesetslist; do
         bloburi=$(az rest --method post --url https://management.azure.com/subscriptions/${subscription}/resourceGroups/${1}/providers/Microsoft.Compute/virtualMachineScaleSets/${scaleset}/virtualmachines/$instanceid/retrieveBootDiagnosticsData?api-version=2022-03-01 | jq '.serialConsoleLogBlobUri' -r)
         sleep 4
         curl -sL -o "./${scaleset}-${instanceid}.log" $bloburi
-        echo $(realpath "./${scaleset}-${instanceid}.log")
+        realpath "./${scaleset}-${instanceid}.log"
     done
 done
