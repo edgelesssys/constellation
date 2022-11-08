011f9c597d7a125bbe31bf73e2641a6ee280eeb3
Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
2022-11-08 18:32:59 +0100

    Bring in changes from release branch (#479)
    * Bump version to v2.2.0
    
    * Update changelog
    
    * Fix release detection in pipeline
    
    * Fix PKI selection in pipeline
    
    * Set enforced measurements for AWS
    
    * Update default images
    
    * Fix release docs
    
    * Update mini-con defaults
    
    * Fix measurements action
    
    * Fix syft env variable naming
    
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- internal/config/config.go
+++ internal/config/config.go
@@ -268,7 +268,7 @@ func Default() *Config {
 				IAMProfileControlPlane: "",
 				IAMProfileWorkerNodes:  "",
 				Measurements:           copyPCRMap(awsPCRs),
-				EnforcedMeasurements:   []uint32{}, // TODO: add default values
+				EnforcedMeasurements:   []uint32{4, 8, 9, 11, 12, 13, 15},
 			},
 			Azure: &AzureConfig{
 				SubscriptionID:       "",
@@ -298,7 +298,7 @@ func Default() *Config {
 				EnforcedMeasurements:  []uint32{0, 4, 8, 9, 11, 12, 13, 15},
 			},
 			QEMU: &QEMUConfig{
-				ImageFormat:           "qcow2",
+				ImageFormat:           "raw",
 				VCPUs:                 2,
 				Memory:                2048,
 				MetadataAPIImage:      versions.QEMUMetadataImage,
@@ -306,7 +306,7 @@ func Default() *Config {
 				LibvirtContainerImage: versions.LibvirtImage,
 				Measurements:          copyPCRMap(qemuPCRs),
 				EnforcedMeasurements:  []uint32{4, 8, 9, 11, 12, 13, 15},
-				NVRAM:                 "testing",
+				NVRAM:                 "production",
 			},
 		},
 		KubernetesVersion: string(versions.Default),
