23b0916183333f6e6321db1781a4be071e5587ae
Fabian Kammel <fk@edgeless.systems>
2022-11-16 10:16:24 +0000

    dummy change to trigger linter
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- cli/internal/cmd/version.go
+++ cli/internal/cmd/version.go
@@ -18,7 +18,7 @@ func NewVersionCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Display version of this CLI",
-		Long:  "Display version of this CLI.",
+		Long:  "Display version of this CLI",
 		Args:  cobra.NoArgs,
 		Run:   runVersion,
 	}
