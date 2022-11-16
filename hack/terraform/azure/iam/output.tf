dfe7f855cd8d57b345b1e5235ea1d56df06a9bf0
Moritz Sanft <58110325+stdoutput@users.noreply.github.com>
2022-11-16 20:19:10 +0100

    AB#2578 Implement Azure IAM in terraform (#562)
    * AB#2578 Azure IAM init
    
    * AB#2578 Fixed application owner privileges, added docs
    
    * Add all supported providers to TF lockfile
    
    * Using service principal for role assignment in cluster resource group
    
    Co-authored-by: Malte Poll <mp@edgeless.systems>
    
    * Rephrased header for Azure
    
    Co-authored-by: Malte Poll <mp@edgeless.systems>
    
    * Registry -> Registration typo
    
    Co-authored-by: Malte Poll <mp@edgeless.systems>
    
    * Download lockfile
    
    * File name casing
    
    Co-authored-by: Malte Poll <mp@edgeless.systems>

---
+++ hack/terraform/azure/iam/output.tf
@@ -0,0 +1,28 @@
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
+
+output "tenant_id" {
+  value = data.azurerm_subscription.current.tenant_id
+}
+
+output "region" {
+  value = var.region
+}
+
+output "base_resource_group_name" {
+  value = var.resource_group_name
+}
+
+output "application_id" {
+  value = azuread_application.base_application.application_id
+}
+
+output "uami_id" {
+  value = azurerm_user_assigned_identity.identity_uami.id
+}
+
+output "application_client_secret_value" {
+  value     = azuread_application_password.base_application_secret.value
+  sensitive = true
+}
---
+++ hack/terraform/azure/iam/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "service_principal_name" {
+  type        = string
+  description = "Service principal name"
+}
+
+variable "region" {
+  type        = string
+  description = "Azure resource location"
+}
