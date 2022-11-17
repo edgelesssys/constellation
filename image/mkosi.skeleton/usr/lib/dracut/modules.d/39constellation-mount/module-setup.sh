58d517aeec00697bf2ee22823ecaa69c97617db8
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-11-17 10:24:38 +0100

    image: remove bash options from sourced scripts
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- image/mkosi.skeleton/usr/lib/dracut/modules.d/39constellation-mount/module-setup.sh
+++ image/mkosi.skeleton/usr/lib/dracut/modules.d/39constellation-mount/module-setup.sh
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
+# Note: This script is sourced.
+
 depends() {
   # expands to: systemd systemd-hostnamed systemd-networkd systemd-resolved systemd-timedated systemd-timesyncd
   echo systemd-network-management
