1cead4532522da0862fb49f085eab4d18ed76bd0
stdoutput <moritz.sanft@outlook.de>
2022-11-21 08:31:15 +0100

    Remove IAM input variables from output

--- hack/terraform/gcp/iam/output.tf
+++ hack/terraform/gcp/iam/output.tf
@@ -2,15 +2,3 @@ output "sa_key" {
   value     = google_service_account_key.service_account_key.private_key
   sensitive = true
 }
-
-output "region" {
-  value = var.region
-}
-
-output "zone" {
-  value = var.zone
-}
-
-output "project_id" {
-  value = var.project_id
-}
