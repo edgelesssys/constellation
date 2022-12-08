d3873988c94831355a142a624e9bc53baf9e951d
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2022-12-08 16:55:56 +0100

    ci: fix download scripts for serial logs
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- .github/actions/constellation_create/gcp-logs.sh
+++ .github/actions/constellation_create/gcp-logs.sh
@@ -3,25 +3,49 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-controlInstanceGroup=$(terraform show -json | jq -r .'values.root_module.child_modules[] | select(.address == "module.instance_group_control_plane") | .resources[0].values.base_instance_name')
-workerInstanceGroup=$(terraform show -json | jq -r .'values.root_module.child_modules[] | select(.address == "module.instance_group_worker") | .resources[0].values.base_instance_name')
-zone=$(terraform show -json | jq -r .'values.root_module.child_modules[] | select(.address == "module.instance_group_control_plane") | .resources[0].values.zone')
+pushd constellation-terraform
+controlInstanceGroup=$(
+  terraform show -json |
+    jq -r .'values.root_module.child_modules[] |
+    select(.address == "module.instance_group_control_plane") |
+    .resources[0].values.base_instance_name'
+)
+workerInstanceGroup=$(
+  terraform show -json |
+    jq -r .'values.root_module.child_modules[] |
+    select(.address == "module.instance_group_worker") |
+     .resources[0].values.base_instance_name'
+)
+zone=$(
+  terraform show -json |
+    jq -r .'values.root_module.child_modules[] |
+    select(.address == "module.instance_group_control_plane") |
+     .resources[0].values.zone'
+)
+popd
 
-controlInstanceGroup=${controlInstanceGroup##*/}
-workerInstanceGroupShort=${workerInstanceGroup##*/}
+controlInstances=$(
+  gcloud compute instance-groups managed list-instances "${controlInstanceGroup##*/}" \
+    --zone "${zone}" \
+    --format=json |
+    jq -r '.[] | .instance'
+)
+workerInstances=$(
+  gcloud compute instance-groups managed list-instances "${workerInstanceGroup##*/}" \
+    --zone "${zone}" \
+    --format=json |
+    jq -r '.[] | .instance'
+)
 
-controlInstances=$(gcloud compute instance-groups managed list-instances "${controlInstanceGroup}" --zone "${zone}" --format=json | jq -r '.[] | .instance')
-workerInstances=$(gcloud compute instance-groups managed list-instances "${workerInstanceGroupShort}" --zone "${zone}" --format=json | jq -r '.[] | .instance')
-
-ALL_INSTANCES="${controlInstances} ${workerInstances}"
+allInstances="${controlInstances} ${workerInstances}"
 
 printf "Fetching logs for %s and %s\n" "${controlInstances}" "${workerInstances}"
 
-for INSTANCE in ${ALL_INSTANCES}; do
-  SHORT_NAME=${INSTANCE##*/}
-  printf "Fetching for %s\n" "${SHORT_NAME}"
-  gcloud compute instances get-serial-port-output "${INSTANCE}" \
+for instance in ${allInstances}; do
+  shortName=${instance##*/}
+  printf "Fetching for %s\n" "${shortName}"
+  gcloud compute instances get-serial-port-output "${instance}" \
     --port 1 \
     --start 0 \
-    --zone "${zone}" > "${SHORT_NAME}".log
+    --zone "${zone}" > "${shortName}".log
 done
