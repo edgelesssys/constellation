f19a90527bc52ad8c4c64e130976047226bfa67c
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-01-03 09:52:06 +0100

    cli: use versionsapi fetcher to fetch image ref
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- internal/image/image_test.go
+++ internal/image/image_test.go
@@ -7,16 +7,16 @@ SPDX-License-Identifier: AGPL-3.0-only
 package image
 
 import (
-	"bytes"
 	"context"
-	"io"
+	"encoding/json"
+	"errors"
 	"net/http"
-	"os"
-	"strings"
 	"testing"
 
 	"github.com/edgelesssys/constellation/v2/internal/cloud/cloudprovider"
 	"github.com/edgelesssys/constellation/v2/internal/config"
+	"github.com/edgelesssys/constellation/v2/internal/file"
+	"github.com/edgelesssys/constellation/v2/internal/versionsapi"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -29,40 +29,59 @@ func TestMain(m *testing.M) {
 
 func TestGetReference(t *testing.T) {
 	testCases := map[string]struct {
-		info          *imageInfo
-		csp, variant  string
+		info          versionsapi.ImageInfo
+		provider      cloudprovider.Provider
+		variant       string
 		wantReference string
 		wantErr       bool
 	}{
-		"reference exists": {
-			info:          &imageInfo{AWS: map[string]string{"someVariant": "someReference"}},
-			csp:           "aws",
+		"reference exists aws": {
+			info:          versionsapi.ImageInfo{AWS: map[string]string{"someVariant": "someReference"}},
+			provider:      cloudprovider.AWS,
+			variant:       "someVariant",
+			wantReference: "someReference",
+		},
+		"reference exists azure": {
+			info:          versionsapi.ImageInfo{Azure: map[string]string{"someVariant": "someReference"}},
+			provider:      cloudprovider.Azure,
+			variant:       "someVariant",
+			wantReference: "someReference",
+		},
+		"reference exists gcp": {
+			info:          versionsapi.ImageInfo{GCP: map[string]string{"someVariant": "someReference"}},
+			provider:      cloudprovider.GCP,
+			variant:       "someVariant",
+			wantReference: "someReference",
+		},
+		"reference exists qemu": {
+			info:          versionsapi.ImageInfo{QEMU: map[string]string{"someVariant": "someReference"}},
+			provider:      cloudprovider.QEMU,
 			variant:       "someVariant",
 			wantReference: "someReference",
 		},
 		"csp does not exist": {
-			info:    &imageInfo{AWS: map[string]string{"someVariant": "someReference"}},
-			csp:     "nonExistingCSP",
-			variant: "someVariant",
-			wantErr: true,
+			info:     versionsapi.ImageInfo{AWS: map[string]string{"someVariant": "someReference"}},
+			provider: cloudprovider.Unknown,
+			variant:  "someVariant",
+			wantErr:  true,
 		},
 		"variant does not exist": {
-			info:    &imageInfo{AWS: map[string]string{"someVariant": "someReference"}},
-			csp:     "aws",
-			variant: "nonExistingVariant",
-			wantErr: true,
+			info:     versionsapi.ImageInfo{AWS: map[string]string{"someVariant": "someReference"}},
+			provider: cloudprovider.AWS,
+			variant:  "nonExistingVariant",
+			wantErr:  true,
 		},
-		"info is nil": {
-			info:    nil,
-			csp:     "aws",
-			variant: "someVariant",
-			wantErr: true,
+		"info is empty": {
+			info:     versionsapi.ImageInfo{},
+			provider: cloudprovider.AWS,
+			variant:  "someVariant",
+			wantErr:  true,
 		},
 		"csp is nil": {
-			info:    &imageInfo{AWS: nil},
-			csp:     "aws",
-			variant: "someVariant",
-			wantErr: true,
+			info:     versionsapi.ImageInfo{AWS: nil},
+			provider: cloudprovider.AWS,
+			variant:  "someVariant",
+			wantErr:  true,
 		},
 	}
 
@@ -71,7 +90,7 @@ func TestGetReference(t *testing.T) {
 			assert := assert.New(t)
 			require := require.New(t)
 
-			reference, err := tc.info.getReference(tc.csp, tc.variant)
+			reference, err := getReferenceFromImageInfo(tc.provider, tc.variant, tc.info)
 
 			if tc.wantErr {
 				assert.Error(err)
@@ -81,14 +100,6 @@ func TestGetReference(t *testing.T) {
 			assert.Equal(tc.wantReference, reference)
 		})
 	}
-}
-
-func TestGetReferenceOnNil(t *testing.T) {
-	assert := assert.New(t)
-
-	var lut *imageInfo
-	_, err := lut.getReference("someCSP", "someVariant")
-	assert.Error(err)
 }
 
 func TestVariant(t *testing.T) {
@@ -161,55 +172,104 @@ func TestVariant(t *testing.T) {
 
 func TestFetchReference(t *testing.T) {
 	img := "ref/abc/stream/nightly/v1.2.3"
-	client := newTestClient(func(req *http.Request) *http.Response {
-		if strings.HasSuffix(req.URL.String(), "/constellation/v1/ref/abc/stream/nightly/image/v1.2.3/info.json") {
-			return &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       io.NopCloser(bytes.NewBufferString(lut)),
-				Header:     make(http.Header),
-			}
+	newImgInfo := func() versionsapi.ImageInfo {
+		return versionsapi.ImageInfo{
+			Ref:     "abc",
+			Stream:  "nightly",
+			Version: "v1.2.3",
+			QEMU:    map[string]string{"default": "someReference"},
+			AWS:     map[string]string{"foo": "bar"},
+			Azure:   map[string]string{"foo": "bar"},
+			GCP:     map[string]string{"foo": "bar"},
 		}
-		return &http.Response{
-			StatusCode: http.StatusNotFound,
-			Body:       io.NopCloser(bytes.NewBufferString("Not found.")),
-			Header:     make(http.Header),
-		}
-	})
+	}
+	imgInfoPath := imageInfoFilename(newImgInfo())
 
 	testCases := map[string]struct {
-		config        *config.Config
-		overrideFile  string
-		wantReference string
-		wantErr       bool
+		config           *config.Config
+		imageInfoFetcher versionsAPIImageInfoFetcher
+		localFile        []byte
+		wantReference    string
+		wantErr          bool
 	}{
 		"reference fetched remotely": {
-			config: &config.Config{Image: img, Provider: config.ProviderConfig{
-				QEMU: &config.QEMUConfig{},
-			}},
+			config: &config.Config{
+				Image:    img,
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
+			imageInfoFetcher: &stubVersionsAPIImageFetcher{
+				fetchImageInfoInfo: newImgInfo(),
+			},
 			wantReference: "someReference",
 		},
-		"reference fetched locally": {
-			config: &config.Config{Image: img, Provider: config.ProviderConfig{
-				QEMU: &config.QEMUConfig{},
-			}},
-			overrideFile:  `{"qemu":{"default":"localOverrideReference"}}`,
-			wantReference: "localOverrideReference",
-		},
-		"lut is invalid": {
-			config: &config.Config{Image: img, Provider: config.ProviderConfig{
-				QEMU: &config.QEMUConfig{},
-			}},
-			overrideFile: `{`,
-			wantErr:      true,
-		},
-		"image version does not exist": {
-			config: &config.Config{Image: "nonExistingImageName", Provider: config.ProviderConfig{
-				QEMU: &config.QEMUConfig{},
-			}},
+		"reference fetched remotely fails": {
+			config: &config.Config{
+				Image:    img,
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
+			imageInfoFetcher: &stubVersionsAPIImageFetcher{
+				fetchImageInfoErr: errors.New("failed"),
+			},
 			wantErr: true,
 		},
-		"invalid config": {
-			config:  &config.Config{},
+		"reference fetched locally": {
+			config: &config.Config{
+				Image:    img,
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
+			localFile: func() []byte {
+				info := newImgInfo()
+				info.QEMU["default"] = "localOverrideReference"
+				file, err := json.Marshal(info)
+				require.NoError(t, err)
+				return file
+			}(),
+			wantReference: "localOverrideReference",
+		},
+		"local file first": {
+			config: &config.Config{
+				Image:    img,
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
+			imageInfoFetcher: &stubVersionsAPIImageFetcher{
+				fetchImageInfoInfo: newImgInfo(),
+			},
+			localFile: func() []byte {
+				info := newImgInfo()
+				info.QEMU["default"] = "localOverrideReference"
+				file, err := json.Marshal(info)
+				require.NoError(t, err)
+				return file
+			}(),
+			wantReference: "localOverrideReference",
+		},
+		"local file is invalid": {
+			config: &config.Config{
+				Image:    img,
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
+			localFile: []byte("invalid"),
+			wantErr:   true,
+		},
+		"local file has invalid image info": {
+			config: &config.Config{
+				Image:    img,
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
+			localFile: func() []byte {
+				info := newImgInfo()
+				info.Ref = ""
+				file, err := json.Marshal(info)
+				require.NoError(t, err)
+				return file
+			}(),
+			wantErr: true,
+		},
+		"image version does not exist": {
+			config: &config.Config{
+				Image:    "nonExistingImageName",
+				Provider: config.ProviderConfig{QEMU: &config.QEMUConfig{}},
+			},
 			wantErr: true,
 		},
 	}
@@ -219,11 +279,20 @@ func TestFetchReference(t *testing.T) {
 			assert := assert.New(t)
 			require := require.New(t)
 
-			fetcher := &Fetcher{
-				httpc: client,
-				fs:    newImageVersionStubFs(t, img, tc.overrideFile),
+			fs := afero.NewMemMapFs()
+			af := &afero.Afero{Fs: fs}
+			if tc.localFile != nil {
+				fh := file.NewHandler(af)
+				require.NoError(fh.Write(imgInfoPath, tc.localFile))
 			}
+
+			fetcher := &Fetcher{
+				fetcher: tc.imageInfoFetcher,
+				fs:      af,
+			}
+
 			reference, err := fetcher.FetchReference(context.Background(), tc.config)
+
 			if tc.wantErr {
 				assert.Error(err)
 				return
@@ -232,6 +301,17 @@ func TestFetchReference(t *testing.T) {
 			assert.Equal(tc.wantReference, reference)
 		})
 	}
+}
+
+type stubVersionsAPIImageFetcher struct {
+	fetchImageInfoInfo versionsapi.ImageInfo
+	fetchImageInfoErr  error
+}
+
+func (f *stubVersionsAPIImageFetcher) FetchImageInfo(ctx context.Context, imageInfo versionsapi.ImageInfo) (
+	versionsapi.ImageInfo, error,
+) {
+	return f.fetchImageInfoInfo, f.fetchImageInfoErr
 }
 
 func must(t *testing.T, err error) {
@@ -255,15 +335,3 @@ func newTestClient(fn roundTripFunc) *http.Client {
 		Transport: fn,
 	}
 }
-
-func newImageVersionStubFs(t *testing.T, image string, overrideFile string) *afero.Afero {
-	fs := afero.NewMemMapFs()
-	img, err := newImageName(image)
-	must(t, err)
-	if overrideFile != "" {
-		must(t, afero.WriteFile(fs, img.filename(), []byte(overrideFile), os.ModePerm))
-	}
-	return &afero.Afero{Fs: fs}
-}
-
-const lut = `{"qemu":{"default":"someReference"}}`
