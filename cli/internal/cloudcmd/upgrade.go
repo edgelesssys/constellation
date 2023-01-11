ff85a0068b89e41a3f981bad3d22463591c2df19
Fabian Kammel <fk@edgeless.systems>
2023-01-11 16:07:07 +0100

    upgrade: fix broken reference from constellation-os to constellation-version (#939)
    * update constellation-os to constellation-version references
    * update nodeimage to nodeversion in CRD type name
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>
    Co-authored-by: Malte Poll <mp@edgeless.systems>
    (cherry picked from commit 82a0fcbb9dd95acf4249766e22a648ffb693acab)

--- cli/internal/cloudcmd/upgrade.go
+++ cli/internal/cloudcmd/upgrade.go
@@ -82,7 +82,7 @@ func (u *Upgrader) Upgrade(ctx context.Context, imageReference, imageVersion str
 
 // GetCurrentImage returns the currently used image version of the cluster.
 func (u *Upgrader) GetCurrentImage(ctx context.Context) (*unstructured.Unstructured, string, error) {
-	imageStruct, err := u.dynamicInterface.getCurrent(ctx, "constellation-os")
+	imageStruct, err := u.dynamicInterface.getCurrent(ctx, "constellation-version")
 	if err != nil {
 		return nil, "", err
 	}
@@ -198,7 +198,7 @@ func (u *dynamicClient) getCurrent(ctx context.Context, name string) (*unstructu
 	return u.client.Resource(schema.GroupVersionResource{
 		Group:    "update.edgeless.systems",
 		Version:  "v1alpha1",
-		Resource: "nodeimages",
+		Resource: "nodeversions",
 	}).Get(ctx, name, metav1.GetOptions{})
 }
 
@@ -207,7 +207,7 @@ func (u *dynamicClient) update(ctx context.Context, obj *unstructured.Unstructur
 	return u.client.Resource(schema.GroupVersionResource{
 		Group:    "update.edgeless.systems",
 		Version:  "v1alpha1",
-		Resource: "nodeimages",
+		Resource: "nodeversions",
 	}).Update(ctx, obj, metav1.UpdateOptions{})
 }
 
