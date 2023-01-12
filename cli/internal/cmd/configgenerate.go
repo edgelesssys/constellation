0b12950def4a1827ead568a7e7397894e604cff9
stdoutput <moritz.sanft@outlook.de>
2023-01-12 11:08:10 +0100

    AB#2706 remove unnecessary if clause

--- cli/internal/cmd/configgenerate.go
+++ cli/internal/cmd/configgenerate.go
@@ -80,12 +80,11 @@ func (cg *configGenerateCmd) configGenerate(cmd *cobra.Command, fileHandler file
 		return err
 	}
 
-	if flags.file != "-" {
-		cmd.Println("Config file written to", flags.file)
-		cmd.Println("Please fill in your CSP-specific configuration before proceeding.")
-		cmd.Println("For more information refer to the documentation:")
-		cmd.Println("\thttps://docs.edgeless.systems/constellation/getting-started/first-steps")
-	}
+	cmd.Println("Config file written to", flags.file)
+	cmd.Println("Please fill in your CSP-specific configuration before proceeding.")
+	cmd.Println("For more information refer to the documentation:")
+	cmd.Println("\thttps://docs.edgeless.systems/constellation/getting-started/first-steps")
+
 	return nil
 }
 
