174bff4db5d5771ad0e1b4e8b4448a1f1b0ed5ed
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-01-13 16:31:22 +0100

    cp
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- .github/actions/versionsapi/entrypoint.sh
+++ .github/actions/versionsapi/entrypoint.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env bash
 
-set -x
-
-for arg in "$@"; do
-  if [[ ${arg} != "" ]]; then
-    args[${#args[@]}]="${arg}"
-  fi
-done
-
-out=$(/versionsapi "${args[@]}")
-if [[ $? -ne 0 ]]; then
-  exit 1
-fi
-
-if [[ ${GITHUB_ACTIONS} == "true" ]]; then
-  echo "output=test" | tee "${GITHUB_OUTPUT}"
-else
-  echo "${out}"
-fi
+sleep 1000
