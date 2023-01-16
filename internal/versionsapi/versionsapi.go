86c5ff8dfbc25d2b33e1661215587e27c60bb820
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-01-16 12:10:10 +0100

    dev-docs: add Go package docs
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- internal/versionsapi/versionsapi.go
+++ internal/versionsapi/versionsapi.go
@@ -7,8 +7,14 @@ SPDX-License-Identifier: AGPL-3.0-only
 /*
 # Versions API
 
-The Versions API is a REST API that provides information about image versions.
+The Versions API is a provides information about versions of Constellation components.
 
-The API can be used to get a list of all available image versions for a given branch and update stream.
+This package defines API types that represents objects of the versions API.
+The types provide helper methods for validation and commonly used operations on the
+information contained in the objects. Especially the paths used for the API are defined
+in these helper methods.
+
+The package also provides helper functions that can be used in context of the versions API,
+e.g. to validate versions.
 */
 package versionsapi
