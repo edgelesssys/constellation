2f2e79381042846c718fa044d3b62829dd1335e2
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-01-23 08:53:25 +0100

    AB#2834 add go package doc to iamid (#1054)

--- cli/internal/iamid/id.go
+++ cli/internal/iamid/id.go
@@ -3,6 +3,9 @@ Copyright (c) Edgeless Systems GmbH
 SPDX-License-Identifier: AGPL-3.0-only
 */
 
+/*
+Package iamid contains the output information of IAM resource creation.
+*/
 package iamid
 
 import (
