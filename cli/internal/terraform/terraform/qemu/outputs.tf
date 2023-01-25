d2369aceeffd6e81f7854d8a8880892f10da43a7
Malte Poll <mp@edgeless.systems>
2023-01-25 12:05:00 +0100

    cli: set placeholder uid for QEMU / MiniConstellation

--- cli/internal/terraform/terraform/qemu/outputs.tf
+++ cli/internal/terraform/terraform/qemu/outputs.tf
@@ -2,6 +2,10 @@ output "ip" {
   value = module.control_plane.instance_ips[0]
 }
 
+output "uid" {
+  value = "qemu" // placeholder
+}
+
 output "initSecret" {
   value     = random_password.initSecret.result
   sensitive = true
