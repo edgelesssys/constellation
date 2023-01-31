b58c112144b20ade8a0c030f1d67dec97a9878f0
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-01-31 16:15:31 +0100

    build: add shell.nix
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{pkgs ? import <nixpkgs> {}}:
+(pkgs.buildFHSUserEnv {
+  name = "bazel";
+  targetPkgs = pkgs: [
+    pkgs.bazel_6
+    pkgs.glibc
+    pkgs.gcc
+    pkgs.jdk11 # TODO(katexochen): investigate why our build chain doesn't work on NixOS
+  ];
+})
+.env
