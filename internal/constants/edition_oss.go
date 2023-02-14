225dd598ece3f435a448edb9fde1d37d8f924abb
Malte Poll <mp@edgeless.systems>
2023-02-14 11:15:07 +0100

    cli: add stamping as alternative build info source

---
+++ internal/constants/edition_oss.go
@@ -0,0 +1,12 @@
+//go:build !enterprise
+
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package constants
+
+// Edition is the edition of the CLI.
+var Edition = "Community"
