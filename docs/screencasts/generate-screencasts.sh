2b27c9f792c6d24dbd512681573105d423f31025
Fabian Kammel <fk@edgeless.systems>
2023-02-15 14:56:30 +0000

    Fix shellcheck issues.
    Signed-off-by: Fabian Kammel <fk@edgeless.systems>

--- docs/screencasts/generate-screencasts.sh
+++ docs/screencasts/generate-screencasts.sh
@@ -28,7 +28,7 @@ cp recordings/check-sbom.cast ../static/assets/check-sbom.cast
 
 # Create config
 docker run -it \
-    -v $HOME/.config/gcloud:/root/.config/gcloud \
+    -v "${HOME}"/.config/gcloud:/root/.config/gcloud \
     -v "$(pwd)"/recordings:/recordings \
     -v "$(pwd)"/constellation:/constellation \
     screenrecodings /scripts/configure-cluster.expect
@@ -36,7 +36,7 @@ cp recordings/configure-cluster.cast ../static/assets/configure-cluster.cast
 
 # Create cluster
 docker run -it \
-    -v $HOME/.config/gcloud:/root/.config/gcloud \
+    -v "${HOME}"/.config/gcloud:/root/.config/gcloud \
     -v "$(pwd)"/recordings:/recordings \
     -v "$(pwd)"/constellation:/constellation \
     screenrecodings /scripts/create-cluster.expect
@@ -44,7 +44,7 @@ cp recordings/create-cluster.cast ../static/assets/create-cluster.cast
 
 # Terminate cluster
 docker run -it \
-    -v $HOME/.config/gcloud:/root/.config/gcloud \
+    -v "${HOME}"/.config/gcloud:/root/.config/gcloud \
     -v "$(pwd)"/recordings:/recordings \
     -v "$(pwd)"/constellation:/constellation \
     screenrecodings /scripts/terminate-cluster.expect
@@ -52,7 +52,7 @@ cp recordings/terminate-cluster.cast ../static/assets/terminate-cluster.cast
 
 # Delete IAM
 docker run -it \
-    -v $HOME/.config/gcloud:/root/.config/gcloud \
+    -v "${HOME}"/.config/gcloud:/root/.config/gcloud \
     -v "$(pwd)"/recordings:/recordings \
     -v "$(pwd)"/constellation:/constellation \
     screenrecodings /scripts/delete-iam.expect
