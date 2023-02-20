8e71cd97a7864c4ceb395f6862d48be3c3ce777d
Otto Bittner <cobittner@posteo.net>
2023-02-20 09:53:46 +0100

    cli: add `config kubernetes-versions` subcommand
    Allows users to learn which k8s versions are supported by the
    current CLI.

---
+++ cli/internal/cmd/configkubernetesversions.go
@@ -0,0 +1,34 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package cmd
+
+import (
+	"strings"
+
+	"github.com/edgelesssys/constellation/v2/internal/versions"
+	"github.com/spf13/cobra"
+)
+
+func newConfigKubernetesVersionsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "kubernetes-versions",
+		Short: "Print the Kubernetes versions supported by this CLI",
+		Long:  "Print the Kubernetes versions supported by this CLI.",
+		Args:  cobra.ArbitraryArgs,
+		Run:   printSupportedKubernetesVersions,
+	}
+
+	return cmd
+}
+
+func printSupportedKubernetesVersions(cmd *cobra.Command, args []string) {
+	cmd.Printf("Supported Kubernetes Versions:\n\t%s\n", formatKubernetesVersions())
+}
+
+func formatKubernetesVersions() string {
+	return strings.Join(versions.SupportedK8sVersions(), "\n\t")
+}
