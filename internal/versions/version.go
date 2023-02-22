2c0644b16568843a523772d800e6959bbf4f4d88
stdoutput <moritz.sanft@outlook.de>
2023-02-22 14:52:05 +0100

    extend functionality

--- internal/versions/version.go
+++ internal/versions/version.go
@@ -9,30 +9,75 @@ package versions
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
+	"github.com/edgelesssys/constellation/v2/internal/constants"
 	"golang.org/x/mod/semver"
 )
 
 // Version represents a semantic version.
-type Version string
+type Version struct {
+	Major int
+	Minor int
+	Patch int
+}
 
 // NewVersion returns a Version from a string.
 func NewVersion(version string) (Version, error) {
-	if !semver.IsValid(version) {
-		return Version(""), fmt.Errorf("invalid semver: %s", version)
+	// ensure that semver has "v" prefix
+	if !strings.HasPrefix(version, "v") {
+		version = "v" + version
 	}
 
-	return Version(version), nil
+	if !semver.IsValid(version) {
+		return Version{}, fmt.Errorf("invalid semver: %s", version)
+	}
+
+	version = semver.Canonical(version)
+
+	var major, minor, patch int
+	_, err := fmt.Sscanf(version, "v%d.%d.%d", &major, &minor, &patch)
+	if err != nil {
+		return Version{}, fmt.Errorf("parsing semver parts: %w", err)
+	}
+
+	return Version{
+		Major: major,
+		Minor: minor,
+		Patch: patch,
+	}, nil
 }
 
-// String returns the string representation of the Version.
+// String returns the string representation of the version.
 func (v Version) String() string {
-	return string(v)
+	return fmt.Sprintf("v%d.%d.%d", v.Major, v.Minor, v.Patch)
 }
 
 // Compare compares two versions. It relies on the semver.Compare function internally.
 func (v Version) Compare(other Version) int {
 	return semver.Compare(v.String(), other.String())
+}
+
+// CanUpgradeTo returns if a version can be upgraded to another version.
+// It checks if the version of other is greater than the current version and allows a drift of at most one minor version.
+func (v Version) CanUpgradeTo(other Version) bool {
+	return v.Compare(other) < 0 && other.Minor-v.Minor <= 1
+}
+
+// CanUpgradeToBinary returns if a version can upgrade to the version of the current built binary.
+// It checks if the version of the binary is greater than the current version and allows a drift of at most one minor version.
+func (v Version) CanUpgradeToBinary() bool {
+	binaryVersion, err := NewVersion(constants.VersionInfo)
+	if err != nil {
+		return false
+	}
+
+	return v.CanUpgradeTo(binaryVersion)
+}
+
+// NextMinor returns the next minor version in the format "vMAJOR.MINOR".
+func (v Version) NextMinor() string {
+	return fmt.Sprintf("v%d.%d", v.Major, v.Minor+1)
 }
 
 // MarshalJSON implements the json.Marshaler interface.
--- internal/versions/version_test.go
+++ internal/versions/version_test.go
@@ -20,8 +20,9 @@ func TestVersionValidation(t *testing.T) {
 	}{
 		"valid version":    {"v1.18.0", false},
 		"invalid version":  {"v1.18. 0", true},
-		"missing prefix":   {"1.18.0", true},
+		"add prefix":       {"1.18.0", false},
 		"only major.minor": {"v1.18", false},
+		"only major":       {"v1", false},
 	}
 
 	for name, tc := range testCases {
@@ -145,6 +146,101 @@ func TestComparison(t *testing.T) {
 			} else {
 				assert.NoError(err)
 				assert.Equal(tc.want, v1.Compare(v2))
+			}
+		})
+	}
+}
+
+func TestCanUpgrade(t *testing.T) {
+	testCases := map[string]struct {
+		version1 string
+		version2 string
+		want     bool
+		wantErr  bool
+	}{
+		"equal": {
+			version1: "v1.18.0",
+			version2: "v1.18.0",
+			want:     false,
+		},
+		"patch less than": {
+			version1: "v1.18.0",
+			version2: "v1.18.1",
+			want:     true,
+		},
+		"minor less then": {
+			version1: "v1.18.0",
+			version2: "v1.19.0",
+			want:     true,
+		},
+		"minor too big drift": {
+			version1: "v1.18.0",
+			version2: "v1.19.1",
+			want:     true,
+		},
+		"major too big drift": {
+			version1: "v1.18.0",
+			version2: "v2.18.1",
+			want:     true,
+		},
+		"greater than": {
+			version1: "v1.18.1",
+			version2: "v1.18.0",
+			want:     false,
+		},
+		"invalid version": {
+			version1: "v1.18.0",
+			version2: "v1.18. ",
+			wantErr:  true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			v1, err := NewVersion(tc.version1)
+			require.NoError(err)
+
+			v2, err := NewVersion(tc.version2)
+			if tc.wantErr {
+				require.Error(err)
+			} else {
+				assert.NoError(err)
+				assert.Equal(tc.want, v1.CanUpgradeTo(v2))
+			}
+		})
+	}
+}
+
+func TestNextMinor(t *testing.T) {
+	testCases := map[string]struct {
+		version string
+		want    string
+		wantErr bool
+	}{
+		"valid version": {
+			version: "v1.18.2",
+			want:    "v1.19",
+		},
+		"invalid version": {
+			version: "v1.18. ",
+			wantErr: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			v, err := NewVersion(tc.version)
+			if tc.wantErr {
+				require.Error(err)
+			} else {
+				assert.NoError(err)
+				assert.Equal(tc.want, v.NextMinor())
 			}
 		})
 	}
