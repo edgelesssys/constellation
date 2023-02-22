f13da4878dfa85903b5af0da79300a4d895731aa
Otto Bittner <cobittner@posteo.net>
2023-02-22 10:41:21 +0100

    Revert "deps: update Terraform azurerm to v3.44.1 (#1197)"
    This reverts commit 253f833f6c947af9b803e4b8b3466ff54bcb2740.

--- cli/internal/terraform/terraform/iam/azure/main.tf
+++ cli/internal/terraform/terraform/iam/azure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.44.1"
+      version = "3.41.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
