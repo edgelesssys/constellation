350de5a4718041ddc68e09a663a65e879bf790e3
Moritz Eckert <m1gh7ym0@gmail.com>
2023-03-03 18:37:03 +0100

    ci: Add tooling to create benchmark figures

--- .github/actions/e2e_benchmark/README.md
+++ .github/actions/e2e_benchmark/README.md
@@ -41,18 +41,19 @@ Example table:
 
 </details>
 
-### Drawing Performance Charts
-The action also draws graphs as used in the [Constellation docs](https://docs.edgeless.systems/constellation/next/overview/performance). The graphs compare the performance of Constellation to the performance of managed Kubernetes clusters.
-
-Graphs are created with every run of the benchmarking action. The action attaches them to the `benchmark` artifact of the workflow run.
-
 ## Updating Stored Records
 
 ### Managed Kubernetes
 One must manually update the stored benchmark records of managed Kubernetes:
 
 ### AKS
-Follow the [Azure documentation](https://learn.microsoft.com/en-us/azure/aks/learn/quick-kubernetes-deploy-portal?tabs=azure-cli) to create an AKS cluster of desired benchmarking settings (region, instance types). If comparing against Constellation clusters with CVM instances, make sure to select the matching CVM instance type on Azure as well.
+Follow the [Azure documentation](https://learn.microsoft.com/en-us/azure/aks/learn/quick-kubernetes-deploy-portal?tabs=azure-cli) to create an AKS cluster of desired benchmarking settings (region, instance types). If comparing against Constellation clusters with CVM instances, make sure to select the instance type on AKS as well.
+
+For example:
+```bash
+az aks create -g moritz-constellation -n benchmark --node-count 2
+az aks get-credentials -g moritz-constellation -n benchmark
+```
 
 Once the cluster is ready, set up managing access via `kubectl` and take the benchmark:
 ```bash
@@ -63,8 +64,9 @@ install knb /usr/local/bin
 cd ..
 
 # Setup kubestr
-HOSTOS="$(go env GOOS)"
+case "$(go env GOOS)" in "darwin") HOSTOS="MacOS";; *) HOSTOS="$(go env GOOS)";; esac
 HOSTARCH="$(go env GOARCH)"
+KUBESTR_VER=0.4.37
 curl -fsSLO https://github.com/kastenhq/kubestr/releases/download/v${KUBESTR_VER}/kubestr_${KUBESTR_VER}_${HOSTOS}_${HOSTARCH}.tar.gz
 tar -xzf kubestr_${KUBESTR_VER}_${HOSTOS}_${HOSTARCH}.tar.gz
 install kubestr /usr/local/bin
@@ -72,13 +74,13 @@ install kubestr /usr/local/bin
 
 # Run kubestr
 mkdir -p out
-kubestr fio -e "out/fio-constellation-aks.json" -o json -s encrypted-rwo -z 400Gi
+kubestr fio -e "out/fio-AKS.json" -o json -s default -z 400Gi
 
 # Run knb
-workers=$(kubectl get nodes | grep worker)
-server=$(echo $workers | head -1 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')
-client=$(echo $workers | head -2 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')
-knb -f "out/knb-constellation-aks.json" -o json --server-node $server --client-node $client
+workers="$(kubectl get nodes | grep nodepool)"
+server="$(echo $workers | head -1 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')"
+client="$(echo $workers | head -2 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')"
+knb -f "out/knb-AKS.json" -o json --server-node $server --client-node $client
 
 
 # Benchmarks done, do processing.
@@ -86,9 +88,10 @@ knb -f "out/knb-constellation-aks.json" -o json --server-node $server --client-n
 # Parse
 git clone https://github.com/edgelesssys/constellation.git
 mkdir -p benchmarks
-BDIR=benchmarks
-EXT_NAME=AKS
-KBENCH_RESULTS=out/
+export BDIR=benchmarks
+export CSP=azure
+export EXT_NAME=AKS
+export BENCH_RESULTS=out/
 
 python constellation/.github/actions/e2e_benchmark/evaluate/parse.py
 
@@ -98,7 +101,29 @@ aws s3 cp benchmarks/AKS.json ${S3_PATH}/AKS.json
 ```
 
 ### GKE
-Create a GKE cluster of desired benchmarking settings (region, instance types). If comparing against Constellation clusters with CVM instances, make sure to select the matching CVM instance type on GCP and enable **confidential** VMs as well.
+Create a GKE cluster of desired benchmarking settings (region, instance types). If comparing against Constellation clusters with CVM instances, make sure to select the matching instance type on GKE.
+For example:
+
+```bash
+gcloud container clusters create benchmark \
+    --zone europe-west3-b \
+    --node-locations europe-west3-b \
+    --machine-type n2d-standard-4 \
+    --num-nodes 2
+gcloud container clusters get-credentials benchmark --region europe-west3-b
+# create storage class for pd-standard
+cat <<EOF | kubectl apply -f -
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: pd-standard
+provisioner: pd.csi.storage.gke.io
+volumeBindingMode: WaitForFirstConsumer
+allowVolumeExpansion: true
+parameters:
+  type: pd-standard
+EOF
+```
 
 Once the cluster is ready, set up managing access via `kubectl` and take the benchmark:
 ```bash
@@ -109,29 +134,31 @@ install knb /usr/local/bin
 cd ..
 
 # Setup kubestr
-HOSTOS="$(go env GOOS)"
+case "$(go env GOOS)" in "darwin") HOSTOS="MacOS";; *) HOSTOS="$(go env GOOS)";; esac
 HOSTARCH="$(go env GOARCH)"
+KUBESTR_VER=0.4.37
 curl -fsSLO https://github.com/kastenhq/kubestr/releases/download/v${KUBESTR_VER}/kubestr_${KUBESTR_VER}_${HOSTOS}_${HOSTARCH}.tar.gz
 tar -xzf kubestr_${KUBESTR_VER}_${HOSTOS}_${HOSTARCH}.tar.gz
 install kubestr /usr/local/bin
 
 # Run kubestr
 mkdir -p out
-kubestr fio -e "out/fio-constellation-gke.json" -o json -s encrypted-rwo -z 400Gi
+kubestr fio -e "out/fio-GKE.json" -o json -s pd-standard -z 400Gi
 
 # Run knb
-workers=$(kubectl get nodes | grep worker)
-server=$(echo $workers | head -1 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')
-client=$(echo $workers | head -2 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')
-knb -f "out/knb-constellation-gke.json" -o json --server-node $server --client-node $client
+workers="$(kubectl get nodes | grep default-pool)"
+server="$(echo $workers | head -1 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')"
+client="$(echo $workers | head -2 | tail -1 |cut -d ' ' -f1|tr '\n' ' ')"
+knb -f "out/knb-GKE.json" -o json --server-node "$server" --client-node "$client"
 
 
 # Parse
 git clone https://github.com/edgelesssys/constellation.git
 mkdir -p benchmarks
-BDIR=benchmarks
-EXT_NAME=GKE
-KBENCH_RESULTS=out/
+export BDIR=benchmarks
+export CSP=gcp
+export EXT_NAME=GKE
+export BENCH_RESULTS=out/
 
 python constellation/.github/actions/e2e_benchmark/evaluate/parse.py
 
@@ -142,3 +169,15 @@ aws s3 cp benchmarks/GKE.json ${S3_PATH}/GKE.json
 
 ### Constellation
 The action updates the stored Constellation records for the selected cloud provider when running on the main branch.
+
+## Drawing Performance Charts
+The action also contains the code to draw graphs as used in the [Constellation docs](https://docs.edgeless.systems/constellation/next/overview/performance).
+The graphs compare the performance of Constellation to the performance of managed Kubernetes clusters.
+It expects the results of `[AKS.json, GKE.json, constellation-azure.json, constellation-gcp.json]` to be present in the `BDIR` folder.
+
+Graphs can thne be created from using the `graphs.py` script:
+
+```bash
+BDIR=benchmarks
+python ./graph.py
+```
---
+++ .github/actions/e2e_benchmark/evaluate/graph.py
@@ -0,0 +1,229 @@
+"""Generate graphs comparing K-Bench benchmarks across cloud providers and Constellation."""
+import json
+import os
+import tempfile
+from collections import defaultdict
+from pathlib import Path
+from urllib import request
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import font_manager as fm
+
+
+SUBJECTS = [
+    'constellation-azure',
+    'AKS',
+    'constellation-gcp',
+    'GKE',
+]
+
+LEGEND_NAMES = [
+    'Constellation on Azure',
+    'AKS',
+    'Constellation on GCP',
+    'GKE',
+]
+
+BAR_COLORS = ['#90FF99', '#929292', '#8B04DD', '#000000']
+
+FONT_URL = "https://github.com/google/fonts/raw/main/apache/roboto/static/Roboto-Regular.ttf"
+FONT_NAME = "Roboto-Regular.ttf"
+
+# Rotate bar labels by X degrees
+LABEL_ROTATE_BY = 30
+LABEL_FONTSIZE = 9
+
+# Some lookup dictionaries for x axis
+fio_iops_unit = 'IOPS'
+fio_bw_unit = 'KiB/s'
+
+net_unit = 'Mbit/s'
+
+
+def configure() -> str:
+    """Read the benchmark data paths.
+
+    Expects ENV vars (required):
+    - BDIR=benchmarks
+
+    Raises TypeError if at least one of them is missing.
+
+    Returns: out_dir
+    """
+    out_dir = os.environ.get('BDIR', None)
+    if not out_dir:
+        raise TypeError(
+            'ENV variables BDIR is required.')
+    return out_dir
+
+
+def bar_chart(data, title='', unit='', x_label= ''):
+    #     """Draws a bar chart with multiple bars per data point.
+
+    #     Args:
+    #         data (dict[str, list]): Benchmark data dictionary: subject -> lists of value points
+    #         title (str, optional): The title for the chart. Defaults to "".
+    #         suffix (str, optional): The suffix for values e.g. "MiB/s". Defaults to "".
+    #         x_label (str, optional): The label for the x-axis. Defaults to "".
+    #     Returns:
+    #         fig (matplotlib.pyplot.figure): The pyplot figure
+    #     """
+
+    # Create plot and set configs
+    plt.rcdefaults()
+    plt.rc('font', family=FONT_NAME)
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    # Calculate y positions
+    y_pos = np.arange(len(data))
+
+    bars = ax.barh(y_pos, data.values(), align='center', color=BAR_COLORS)
+
+    # Axis formatting
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_color('#DDDDDD')
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.xaxis.grid(True, color='#EEEEEE')
+    ax.yaxis.grid(False)
+
+    # Bar annotations
+    for bar in bars:
+        ax.text(
+            1.03*bar.get_width(),
+            bar.get_y() + bar.get_height() / 2,
+            f'{bar.get_width():.0f}',
+            verticalalignment='center',
+        )
+
+    # Set labels and titles
+    ax.set_yticks(y_pos, labels=data.keys())
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel(x_label, fontdict={"fontsize": 12})
+    if unit != '':
+        unit = f"({unit})"
+    ax.set_title(f'{title} {unit}', fontdict={"fontsize": 20, 'weight': 'bold'})
+
+    plt.tight_layout()
+    #plt.show()
+    return fig
+
+def main():
+    """ Download and setup fonts"""
+    path = Path(tempfile.mkdtemp())
+    font_path = path / FONT_NAME
+    request.urlretrieve(FONT_URL, font_path)
+
+    font = fm.FontEntry(fname=str(font_path), name=FONT_NAME)
+    fm.fontManager.ttflist.append(font)
+
+    """Read the files and create diagrams."""
+    out_dir = configure()
+    combined_results = defaultdict(dict)
+
+    for test in SUBJECTS:
+        # Read the previous results
+        read_path = os.path.join(
+            out_dir, '{subject}.json'.format(subject=test))
+        try:
+            with open(read_path, 'r') as res_file:
+                combined_results[test].update(json.load(res_file))
+        except OSError as e:
+            raise ValueError(
+                'Failed reading {subject} benchmark records: {e}'.format(subject=test, e=e))
+
+    # Network charts
+    ## P2P TCP
+    net_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        net_data[l] = int(combined_results[s]['knb']['pod2pod']['tcp_bw_mbit'])
+    bar_chart(data=net_data,
+              title='K8S CNI Benchmark - Pod to Pod - TCP - Bandwidth',
+              unit=net_unit,
+              x_label = f" TCP Bandwidth in {net_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_net_p2p_tcp.png')
+    plt.savefig(save_name)
+
+    ## P2P TCP
+    net_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        net_data[l] = int(combined_results[s]['knb']['pod2pod']['udp_bw_mbit'])
+    bar_chart(data=net_data,
+              title='K8S CNI Benchmark - Pod to Pod - UDP - Bandwidth',
+              unit=net_unit,
+              x_label = f" UDP Bandwidth in {net_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_net_p2p_udp.png')
+    plt.savefig(save_name)
+
+
+    ## P2SVC TCP
+    net_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        net_data[l] = int(combined_results[s]['knb']['pod2svc']['tcp_bw_mbit'])
+    bar_chart(data=net_data,
+              title='K8S CNI Benchmark - Pod to Service - TCP - Bandwidth',
+              unit=net_unit,
+              x_label = f" TCP Bandwidth in {net_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_net_p2svc_tcp.png')
+    plt.savefig(save_name)
+
+    ## P2SVC UDP
+    net_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        net_data[l] = int(combined_results[s]['knb']['pod2svc']['udp_bw_mbit'])
+    bar_chart(data=net_data,
+              title='K8S CNI Benchmark - Pod to Service - UDP - Bandwidth',
+              unit=net_unit,
+              x_label = f" UDP Bandwidth in {net_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_net_p2svc_udp.png')
+    plt.savefig(save_name)
+
+    # FIO chart
+    ## Read IOPS
+    fio_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        fio_data[l] = int(combined_results[s]['fio']['read_iops']['iops'])
+    bar_chart(data=fio_data,
+              title='FIO Benchmark - Read - IOPS',
+              x_label = f" Read {fio_iops_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_fio_read_iops.png')
+    plt.savefig(save_name)
+
+    ## Write IOPS
+    fio_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        fio_data[l] = int(combined_results[s]['fio']['write_iops']['iops'])
+    bar_chart(data=fio_data,
+              title='FIO Benchmark - Write - IOPS',
+              x_label = f" Write {fio_iops_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_fio_write_iops.png')
+    plt.savefig(save_name)
+
+    ## Read Bandwidth
+    fio_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        fio_data[l] = int(combined_results[s]['fio']['read_bw']['bw_kbytes'])
+    bar_chart(data=fio_data,
+              title='FIO Benchmark - Read - Bandwidth',
+              unit=fio_bw_unit,
+              x_label = f" Read Bandwidth in {fio_bw_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_fio_read_bw.png')
+    plt.savefig(save_name)
+
+    ## Write Bandwidth
+    fio_data = {}
+    for s, l in zip(SUBJECTS, LEGEND_NAMES):
+        fio_data[l] = int(combined_results[s]['fio']['write_bw']['bw_kbytes'])
+    bar_chart(data=fio_data,
+              title='FIO Benchmark - Write - Bandwidth',
+              unit=fio_bw_unit,
+              x_label = f" Write Bandwidth in {fio_bw_unit} - Higher is better")
+    save_name = os.path.join(out_dir, 'benchmark_fio_write_bw.png')
+    plt.savefig(save_name)
+
+
+if __name__ == '__main__':
+    main()
