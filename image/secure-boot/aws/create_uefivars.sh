1624af0cc780b147128a65468a74b294503d190d
Malte Poll <1780588+malt3@users.noreply.github.com>
2023-03-06 13:29:15 +0100

    image: pin aws uefivars version and install new deps (#1345)

--- image/secure-boot/aws/create_uefivars.sh
+++ image/secure-boot/aws/create_uefivars.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 shopt -s inherit_errexit
 
 TMPDIR=$(mktemp -d /tmp/uefivars-XXXXXXXXXXXXXX)
-git clone https://github.com/awslabs/python-uefivars "${TMPDIR}"
+git clone --branch v1.0.0 https://github.com/awslabs/python-uefivars "${TMPDIR}"
+cd "${TMPDIR}" && git reset 9679002a4392d8e7831d2dbda3fab41ccc5c6b8c --hard
 
 "${TMPDIR}/uefivars.py" -i none -o aws -O "$1" -P "${PKI}"/PK.esl -K "${PKI}"/KEK.esl --db "${PKI}"/db.esl
 
