38fed1ca9111cea3976c426473824e1335e05988
Daniel Weiße <dw@edgeless.systems>
2023-03-08 15:12:29 +0100

    log attestation variant
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- joinservice/cmd/main.go
+++ joinservice/cmd/main.go
@@ -48,8 +48,11 @@ func main() {
 	flag.Parse()
 
 	log := logger.New(logger.JSONLog, logger.VerbosityFromInt(*verbosity))
-	log.With(zap.String("version", constants.VersionInfo()), zap.String("cloudProvider", *provider)).
-		Infof("Constellation Node Join Service")
+	log.With(
+		zap.String("version", constants.VersionInfo()),
+		zap.String("cloudProvider", *provider),
+		zap.String("attestationVariant", *attestationVariant),
+	).Infof("Constellation Node Join Service")
 
 	handler := file.NewHandler(afero.NewOsFs())
 
