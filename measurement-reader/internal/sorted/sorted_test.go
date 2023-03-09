d58b5f1c06c2549b73bd48867a396a3446bee1b8
Nils Hanke <nils.hanke@outlook.de>
2023-03-09 18:55:05 +0100

    measurement-reader: unify TPM & TDX sorting

--- measurement-reader/internal/sorted/sorted_test.go
+++ measurement-reader/internal/sorted/sorted_test.go
@@ -4,29 +4,74 @@ Copyright (c) Edgeless Systems GmbH
 SPDX-License-Identifier: AGPL-3.0-only
 */
 
-package tdx
+package sorted
 
 import (
 	"bytes"
 	"testing"
 
 	"github.com/edgelesssys/constellation/v2/internal/attestation/measurements"
-	"github.com/edgelesssys/constellation/v2/measurement-reader/internal/sorted"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSortMeasurements(t *testing.T) {
 	testCases := map[string]struct {
-		input measurements.M
-		want  []sorted.Measurement
+		measurementType MeasurementType
+		input           measurements.M
+		want            []Measurement
 	}{
-		"pre sorted": {
+		"pre sorted TPM": {
+			measurementType: TPM,
+			input: measurements.M{
+				0: measurements.WithAllBytes(0x11, measurements.Enforce),
+				1: measurements.WithAllBytes(0x22, measurements.Enforce),
+				2: measurements.WithAllBytes(0x33, measurements.Enforce),
+			},
+			want: []Measurement{
+				{
+					Index: "PCR[00]",
+					Value: bytes.Repeat([]byte{0x11}, 32),
+				},
+				{
+					Index: "PCR[01]",
+					Value: bytes.Repeat([]byte{0x22}, 32),
+				},
+				{
+					Index: "PCR[02]",
+					Value: bytes.Repeat([]byte{0x33}, 32),
+				},
+			},
+		},
+		"unsorted TPM": {
+			measurementType: TPM,
+			input: measurements.M{
+				1: measurements.WithAllBytes(0x22, measurements.Enforce),
+				0: measurements.WithAllBytes(0x11, measurements.Enforce),
+				2: measurements.WithAllBytes(0x33, measurements.Enforce),
+			},
+			want: []Measurement{
+				{
+					Index: "PCR[00]",
+					Value: bytes.Repeat([]byte{0x11}, 32),
+				},
+				{
+					Index: "PCR[01]",
+					Value: bytes.Repeat([]byte{0x22}, 32),
+				},
+				{
+					Index: "PCR[02]",
+					Value: bytes.Repeat([]byte{0x33}, 32),
+				},
+			},
+		},
+		"pre sorted TDX": {
+			measurementType: TDX,
 			input: measurements.M{
 				0: measurements.WithAllBytes(0x11, false),
 				1: measurements.WithAllBytes(0x22, false),
 				2: measurements.WithAllBytes(0x33, false),
 			},
-			want: []sorted.Measurement{
+			want: []Measurement{
 				{
 					Index: "MRTD",
 					Value: bytes.Repeat([]byte{0x11}, 32),
@@ -41,13 +86,14 @@ func TestSortMeasurements(t *testing.T) {
 				},
 			},
 		},
-		"unsorted": {
+		"unsorted TDX": {
+			measurementType: TDX,
 			input: measurements.M{
 				1: measurements.WithAllBytes(0x22, false),
 				0: measurements.WithAllBytes(0x11, false),
 				2: measurements.WithAllBytes(0x33, false),
 			},
-			want: []sorted.Measurement{
+			want: []Measurement{
 				{
 					Index: "MRTD",
 					Value: bytes.Repeat([]byte{0x11}, 32),
@@ -68,7 +114,7 @@ func TestSortMeasurements(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			assert := assert.New(t)
 
-			got := sortMeasurements(tc.input)
+			got := SortMeasurements(tc.input, tc.measurementType)
 			for i := range got {
 				assert.Equal(got[i].Index, tc.want[i].Index)
 				assert.Equal(got[i].Value, tc.want[i].Value)
