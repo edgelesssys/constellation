e29b27364c0c15a9d66b782507f8239551ff9d2c
Malte Poll <mp@edgeless.systems>
2023-03-09 13:20:00 +0100

    deps: add git and go to nix deps
    Also remove workaround for non-hermetic build tools (protoc, perl)

--- shell.nix
+++ shell.nix
@@ -2,11 +2,12 @@
 (pkgs.buildFHSUserEnv {
   name = "bazel";
   targetPkgs = pkgs: [
+    pkgs.git
+    pkgs.go
     pkgs.bazel_6
     pkgs.glibc
     pkgs.gcc
     pkgs.jdk11 # TODO(katexochen): investigate why our build chain doesn't work on NixOS
-    pkgs.libxcrypt # TODO(katexochen): investigate why our build chain doesn't work on NixOS
   ];
 })
 .env
