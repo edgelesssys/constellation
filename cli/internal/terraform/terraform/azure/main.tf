7a5f5a7b91cbc55a7224f3d74a9ba7bda4d2aad8
stdoutput <moritz.sanft@outlook.de>
2023-03-09 14:01:19 +0100

    remove hyphen

--- cli/internal/terraform/terraform/azure/main.tf
+++ cli/internal/terraform/terraform/azure/main.tf
@@ -42,7 +42,7 @@ resource "random_password" "initSecret" {
 }
 
 resource "azurerm_attestation_provider" "attestation_provider" {
-  name                = format("%s-ap", var.name)
+  name                = format("%sap", var.name)
   resource_group_name = var.resource_group
   location            = var.location
 }
