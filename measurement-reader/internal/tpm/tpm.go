0f0a8258868bd349c6aabfb2a8497e7638f35b64
Nils Hanke <nils.hanke@outlook.de>
2023-03-09 18:55:05 +0100

    measurement-reader: unify TPM & TDX sorting

--- measurement-reader/internal/tpm/tpm.go
+++ measurement-reader/internal/tpm/tpm.go
@@ -8,10 +8,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 package tpm
 
 import (
-	"fmt"
-	"sort"
-
-	"github.com/edgelesssys/constellation/v2/internal/attestation/measurements"
 	"github.com/edgelesssys/constellation/v2/internal/attestation/vtpm"
 	"github.com/edgelesssys/constellation/v2/measurement-reader/internal/sorted"
 	tpmClient "github.com/google/go-tpm-tools/client"
@@ -25,26 +21,5 @@ func Measurements() ([]sorted.Measurement, error) {
 		return nil, err
 	}
 
-	return sortMeasurements(m), nil
-}
-
-func sortMeasurements(m measurements.M) []sorted.Measurement {
-	keys := make([]uint32, 0, len(m))
-	for idx := range m {
-		keys = append(keys, idx)
-	}
-	sort.Slice(keys, func(i, j int) bool {
-		return keys[i] < keys[j]
-	})
-
-	var measurements []sorted.Measurement
-	for _, idx := range keys {
-		expected := m[idx].Expected
-		measurements = append(measurements, sorted.Measurement{
-			Index: fmt.Sprintf("PCR[%02d]", idx),
-			Value: expected[:],
-		})
-	}
-
-	return measurements
+	return sorted.SortMeasurements(m, sorted.TPM), nil
 }
