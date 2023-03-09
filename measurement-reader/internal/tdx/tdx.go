3ab3e61d6b4778bd0309c03d6f9a98d2110c787c
Nils Hanke <nils.hanke@outlook.de>
2023-03-09 18:55:05 +0100

    measurement-reader: unify TPM & TDX sorting

--- measurement-reader/internal/tdx/tdx.go
+++ measurement-reader/internal/tdx/tdx.go
@@ -8,10 +8,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 package tdx
 
 import (
-	"fmt"
-	"sort"
-
-	"github.com/edgelesssys/constellation/v2/internal/attestation/measurements"
 	"github.com/edgelesssys/constellation/v2/internal/attestation/tdx"
 	"github.com/edgelesssys/constellation/v2/measurement-reader/internal/sorted"
 )
@@ -23,36 +19,5 @@ func Measurements() ([]sorted.Measurement, error) {
 		return nil, err
 	}
 
-	return sortMeasurements(m), nil
-}
-
-func sortMeasurements(m measurements.M) []sorted.Measurement {
-	keys := make([]uint32, 0, len(m))
-	for idx := range m {
-		keys = append(keys, idx)
-	}
-	sort.Slice(keys, func(i, j int) bool {
-		return keys[i] < keys[j]
-	})
-
-	var measurements []sorted.Measurement
-	for _, idx := range keys {
-		expected := m[idx].Expected
-
-		// Index 0   == MRTD
-		// Index 1-5 == RTMR[0-4]
-		var index string
-		if (idx) == 0 {
-			index = "MRTD"
-		} else {
-			index = fmt.Sprintf("RTMR[%01d]", idx-1)
-		}
-
-		measurements = append(measurements, sorted.Measurement{
-			Index: index,
-			Value: expected[:],
-		})
-	}
-
-	return measurements
+	return sorted.SortMeasurements(m, sorted.TDX), nil
 }
