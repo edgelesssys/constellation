26939369067c6e1b5b5a2476abf843c970ead72f
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-03-17 11:54:47 +0100

    bazel: add target for tfsec
    but don't include in //:check yet, there are to many false positives.
    
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -9,6 +9,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 package main
 
 import (
+	_ "github.com/aquasecurity/tfsec/cmd/tfsec"
 	_ "github.com/katexochen/sh/v3/cmd/shfmt"
 	_ "github.com/rhysd/actionlint/cmd/actionlint"
 	_ "mvdan.cc/gofumpt"
