a6d820d17aacc29f608f66e41d6208ca42334d27
Malte Poll <mp@edgeless.systems>
2023-03-21 15:05:53 +0100

    bazel: use new bazeldnf feature to write rpm rules in a macro

--- tools/dnf-tree.sh
+++ tools/dnf-tree.sh
@@ -3,17 +3,23 @@
 bazel run //:bazeldnf -- fetch \
   --repofile rpm/repo.yaml
 bazel run //:bazeldnf -- rpmtree \
-  --workspace rpm/rpms.bzl \
+  --workspace WORKSPACE.bazel \
+  --to_macro rpm/rpms.bzl%rpms \
   --buildfile rpm/BUILD.bazel \
   --repofile rpm/repo.yaml \
   --name cryptsetup-devel \
   cryptsetup-devel
 bazel run //:bazeldnf -- rpmtree \
-  --workspace rpm/rpms.bzl \
+  --workspace WORKSPACE.bazel \
+  --to_macro rpm/rpms.bzl%rpms \
   --buildfile rpm/BUILD.bazel \
   --repofile rpm/repo.yaml \
   --name libvirt-devel \
   libvirt-devel
+bazel run //:bazeldnf -- prune \
+  --workspace WORKSPACE.bazel \
+  --to_macro rpm/rpms.bzl%rpms \
+  --buildfile rpm/BUILD.bazel
 bazel run //rpm:ldd-cryptsetup
 bazel run //rpm:ldd-libvirt
 bazel run //rpm:ldd-glibc
