4d238b14f5f169ded62ee0c46a7a16baa90638a9
Moritz Eckert <m1gh7ym0@gmail.com>
2023-03-23 11:23:57 +0100

    docs: update benchmarks with v2.6.0
    Co-authored-by: Thomas Tendyck <51411342+thomasten@users.noreply.github.com>

--- .github/actions/e2e_benchmark/evaluate/graph.py
+++ .github/actions/e2e_benchmark/evaluate/graph.py
@@ -29,10 +29,7 @@ BAR_COLORS = ['#90FF99', '#929292', '#8B04DD', '#000000']
 
 FONT_URL = "https://github.com/google/fonts/raw/main/apache/roboto/static/Roboto-Regular.ttf"
 FONT_NAME = "Roboto-Regular.ttf"
-
-# Rotate bar labels by X degrees
-LABEL_ROTATE_BY = 30
-LABEL_FONTSIZE = 9
+FONT_SIZE = 13
 
 # Some lookup dictionaries for x axis
 fio_iops_unit = 'IOPS'
@@ -58,7 +55,7 @@ def configure() -> str:
     return out_dir
 
 
-def bar_chart(data, title='', unit='', x_label= ''):
+def bar_chart(data, title='', unit='', x_label=''):
     #     """Draws a bar chart with multiple bars per data point.
 
     #     Args:
@@ -72,7 +69,7 @@ def bar_chart(data, title='', unit='', x_label= ''):
 
     # Create plot and set configs
     plt.rcdefaults()
-    plt.rc('font', family=FONT_NAME)
+    plt.rc('font', family=FONT_NAME, size=FONT_SIZE)
     fig, ax = plt.subplots(figsize=(10, 5))
 
     # Calculate y positions
@@ -105,11 +102,13 @@ def bar_chart(data, title='', unit='', x_label= ''):
     ax.set_xlabel(x_label, fontdict={"fontsize": 12})
     if unit != '':
         unit = f"({unit})"
-    ax.set_title(f'{title} {unit}', fontdict={"fontsize": 20, 'weight': 'bold'})
+    ax.set_title(f'{title} {unit}', fontdict={
+                 "fontsize": 20, 'weight': 'bold'})
 
     plt.tight_layout()
-    #plt.show()
+    # plt.show()
     return fig
+
 
 def main():
     """ Download and setup fonts"""
@@ -136,91 +135,90 @@ def main():
                 'Failed reading {subject} benchmark records: {e}'.format(subject=test, e=e))
 
     # Network charts
-    ## P2P TCP
+    # P2P TCP
     net_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         net_data[l] = int(combined_results[s]['knb']['pod2pod']['tcp_bw_mbit'])
     bar_chart(data=net_data,
               title='K8S CNI Benchmark - Pod to Pod - TCP - Bandwidth',
               unit=net_unit,
-              x_label = f" TCP Bandwidth in {net_unit} - Higher is better")
+              x_label=f" TCP Bandwidth in {net_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_net_p2p_tcp.png')
     plt.savefig(save_name)
 
-    ## P2P TCP
+    # P2P TCP
     net_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         net_data[l] = int(combined_results[s]['knb']['pod2pod']['udp_bw_mbit'])
     bar_chart(data=net_data,
               title='K8S CNI Benchmark - Pod to Pod - UDP - Bandwidth',
               unit=net_unit,
-              x_label = f" UDP Bandwidth in {net_unit} - Higher is better")
+              x_label=f" UDP Bandwidth in {net_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_net_p2p_udp.png')
     plt.savefig(save_name)
 
-
-    ## P2SVC TCP
+    # P2SVC TCP
     net_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         net_data[l] = int(combined_results[s]['knb']['pod2svc']['tcp_bw_mbit'])
     bar_chart(data=net_data,
               title='K8S CNI Benchmark - Pod to Service - TCP - Bandwidth',
               unit=net_unit,
-              x_label = f" TCP Bandwidth in {net_unit} - Higher is better")
+              x_label=f" TCP Bandwidth in {net_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_net_p2svc_tcp.png')
     plt.savefig(save_name)
 
-    ## P2SVC UDP
+    # P2SVC UDP
     net_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         net_data[l] = int(combined_results[s]['knb']['pod2svc']['udp_bw_mbit'])
     bar_chart(data=net_data,
               title='K8S CNI Benchmark - Pod to Service - UDP - Bandwidth',
               unit=net_unit,
-              x_label = f" UDP Bandwidth in {net_unit} - Higher is better")
+              x_label=f" UDP Bandwidth in {net_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_net_p2svc_udp.png')
     plt.savefig(save_name)
 
     # FIO chart
-    ## Read IOPS
+    # Read IOPS
     fio_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         fio_data[l] = int(combined_results[s]['fio']['read_iops']['iops'])
     bar_chart(data=fio_data,
               title='FIO Benchmark - Read - IOPS',
-              x_label = f" Read {fio_iops_unit} - Higher is better")
+              x_label=f" Read {fio_iops_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_fio_read_iops.png')
     plt.savefig(save_name)
 
-    ## Write IOPS
+    # Write IOPS
     fio_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         fio_data[l] = int(combined_results[s]['fio']['write_iops']['iops'])
     bar_chart(data=fio_data,
               title='FIO Benchmark - Write - IOPS',
-              x_label = f" Write {fio_iops_unit} - Higher is better")
+              x_label=f" Write {fio_iops_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_fio_write_iops.png')
     plt.savefig(save_name)
 
-    ## Read Bandwidth
+    # Read Bandwidth
     fio_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         fio_data[l] = int(combined_results[s]['fio']['read_bw']['bw_kbytes'])
     bar_chart(data=fio_data,
               title='FIO Benchmark - Read - Bandwidth',
               unit=fio_bw_unit,
-              x_label = f" Read Bandwidth in {fio_bw_unit} - Higher is better")
+              x_label=f" Read Bandwidth in {fio_bw_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_fio_read_bw.png')
     plt.savefig(save_name)
 
-    ## Write Bandwidth
+    # Write Bandwidth
     fio_data = {}
     for s, l in zip(SUBJECTS, LEGEND_NAMES):
         fio_data[l] = int(combined_results[s]['fio']['write_bw']['bw_kbytes'])
     bar_chart(data=fio_data,
               title='FIO Benchmark - Write - Bandwidth',
               unit=fio_bw_unit,
-              x_label = f" Write Bandwidth in {fio_bw_unit} - Higher is better")
+              x_label=f" Write Bandwidth in {fio_bw_unit} - Higher is better")
     save_name = os.path.join(out_dir, 'benchmark_fio_write_bw.png')
     plt.savefig(save_name)
 
