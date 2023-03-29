593d6a295569ca16fbdc469b20afb85e56847ab7
miampf <miampf@proton.me>
2023-03-29 18:59:00 +0200

    wrote unit test for Stderr()

--- bootstrapper/internal/journald/journald_test.go
+++ bootstrapper/internal/journald/journald_test.go
@@ -8,6 +8,7 @@ package journald
 
 import (
 	"errors"
+	"io"
 	"os/exec"
 	"testing"
 
@@ -20,6 +21,24 @@ type stubJournaldCommand struct {
 
 func (j *stubJournaldCommand) Start() error {
 	return j.startError
+}
+
+type stubStderrPipe struct {
+	buffer   []byte
+	readErr  error
+	closeErr error
+}
+
+func (s stubStderrPipe) Read(p []byte) (n int, err error) {
+	size := len(s.buffer)
+	if s.readErr != nil {
+		size = 0
+	}
+	return size, s.readErr
+}
+
+func (s stubStderrPipe) Close() error {
+	return s.closeErr
 }
 
 func TestCollect(t *testing.T) {
@@ -52,6 +71,43 @@ func TestCollect(t *testing.T) {
 			_, err := collector.Pipe()
 			if tc.wantErr {
 				assert.Error(err)
+			}
+		})
+	}
+}
+
+func TestStderr(t *testing.T) {
+	someError := errors.New("failed")
+
+	testCases := map[string]struct {
+		stderrPipe io.ReadCloser
+		wantErr    bool
+	}{
+		"success": {
+			stderrPipe: stubStderrPipe{readErr: io.EOF},
+		},
+		"reading error": {
+			stderrPipe: stubStderrPipe{readErr: someError},
+			wantErr:    true,
+		},
+		"close error": {
+			stderrPipe: stubStderrPipe{closeErr: someError, readErr: io.EOF},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			collector := Collector{
+				stderrPipe: tc.stderrPipe,
+			}
+
+			stderrOut, err := collector.Stderr()
+			if tc.wantErr {
+				assert.Error(err)
+			} else {
+				assert.Equal(stderrOut, []byte{})
 			}
 		})
 	}
