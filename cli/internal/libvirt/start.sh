1f81763a2749191b7de8d7c96a14ebf6eada2174
Malte Poll <mp@edgeless.systems>
2023-04-03 17:50:22 +0200

    cli: convert libvirt container image to Bazel

--- cli/internal/libvirt/start.sh
+++ cli/internal/libvirt/start.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+# ensure library cache is up to date
+ldconfig
+
+chown -R tss:root /var/lib/swtpm-localca
+
 # Assign qemu the GID of the host system's 'kvm' group to avoid permission issues for environments defaulting to 660 for /dev/kvm (e.g. Debian-based distros)
 KVM_HOST_GID="$(stat -c '%g' /dev/kvm)"
 groupadd -o -g "${KVM_HOST_GID}" host-kvm
