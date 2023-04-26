acfc134cb1f4bf2c80be485e5b23a760f260b5c4
katexochen <katexochen@users.noreply.github.com>
2023-04-26 16:37:55 +0000

    image: update measurements and image version

--- internal/config/images_enterprise.go
+++ internal/config/images_enterprise.go
@@ -9,6 +9,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 package config
 
 const (
-	// defaultImage is the default image for the enterprise build.
-	defaultImage = "v2.6.0"
+	// defaultImage is the default image to use.
+	defaultImage = "ref/feat-ci-release-backports/stream/nightly/v2.8.0-pre.0.20230426153301-bd5e787b4ec2"
 )
