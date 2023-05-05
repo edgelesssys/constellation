49b7955cab94fa3c37e3d734e89dde0c74fe7a38
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-05-05 11:59:23 +0200

    ci: fix measurement generation on scheduled build
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- internal/config/image.go
+++ internal/config/image.go
@@ -8,5 +8,5 @@ package config
 
 const (
 	// defaultImage is the default image to use.
-	defaultImage = "ref/main/stream/nightly/v2.8.0-pre.0.20230427155146-4cfa7a0306a5"
+	defaultImage = "ref/main/stream/nightly/v2.8.0-pre.0.20230504130006-c3b13178aa89"
 )
