e7e76d751a7413a58ee29432bd72b0bc15abf28c
miampf <miampf@proton.me>
2023-05-10 12:49:57 +0200

    added full streaming support to cli; logs currently are ignored

--- cli/internal/cmd/init.go
+++ cli/internal/cmd/init.go
@@ -260,6 +260,8 @@ func (d *initDoer) Do(ctx context.Context) error {
 		return &nonRetriableError{fmt.Errorf("init call: %w", err)}
 	}
 
+	fail := false
+
 	for {
 		res, err := resp.Recv()
 		if err == io.EOF {
@@ -269,7 +271,15 @@ func (d *initDoer) Do(ctx context.Context) error {
 			return &nonRetriableError{err}
 		}
 
-		d.resp = res // currently doesnt work for log streaming
+		if res.GetInitFailure() != nil {
+			d.log.Debugf("Received init failure from server")
+			fail = true
+			continue
+		}
+
+		if !fail {
+			d.resp = res
+		}
 	}
 
 	return nil
