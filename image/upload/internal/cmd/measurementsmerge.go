293a9d3f71544569dfda4e7044541a0c3f76c2db
Malte Poll <mp@edgeless.systems>
2023-05-23 15:09:14 +0200

    image: add go code to upload image info and measurements

---
+++ image/upload/internal/cmd/measurementsmerge.go
@@ -0,0 +1,85 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/edgelesssys/constellation/v2/internal/attestation/measurements"
+	"github.com/edgelesssys/constellation/v2/internal/logger"
+	"github.com/spf13/cobra"
+)
+
+// newMeasurementsMergeCmd creates a new merge command.
+func newMeasurementsMergeCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "merge [flags] <measurements.json>...",
+		Short: "Merge OS image measurements",
+		Long:  "Merge OS image measurements.",
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  runMergeMeasurements,
+	}
+
+	cmd.SetOut(os.Stdout)
+	cmd.Flags().String("out", "", "Optional path to write the merge result to. If not set, the result is written to stdout.")
+	cmd.Flags().Bool("verbose", false, "Enable verbose output")
+
+	return cmd
+}
+
+func runMergeMeasurements(cmd *cobra.Command, args []string) error {
+	workdir := os.Getenv("BUILD_WORKING_DIRECTORY")
+	if len(workdir) > 0 {
+		must(os.Chdir(workdir))
+	}
+
+	flags, err := parseMergeMeasurementsFlags(cmd)
+	if err != nil {
+		return err
+	}
+
+	log := logger.New(logger.PlainLog, flags.logLevel)
+	log.Debugf("Parsed flags: %+v", flags)
+
+	mergedMeasurements, err := readMeasurementsArgs(args)
+	if err != nil {
+		return fmt.Errorf("merging measurements: reading input files: %w", err)
+	}
+
+	out := cmd.OutOrStdout()
+	if len(flags.out) > 0 {
+		outF, err := os.Create(flags.out)
+		if err != nil {
+			return fmt.Errorf("merging measurements: opening output file: %w", err)
+		}
+		defer outF.Close()
+		out = outF
+	}
+
+	if err := json.NewEncoder(out).Encode(mergedMeasurements); err != nil {
+		return fmt.Errorf("merging measurements: writing output file: %w", err)
+	}
+	log.Infof("Merged image measurements")
+	return nil
+}
+
+func readMeasurementsArgs(paths []string) (measurements.ImageMeasurementsV2, error) {
+	measuremnts := make([]measurements.ImageMeasurementsV2, len(paths))
+	for i, path := range paths {
+		f, err := os.Open(path)
+		if err != nil {
+			return measurements.ImageMeasurementsV2{}, err
+		}
+		defer f.Close()
+		if err := json.NewDecoder(f).Decode(&measuremnts[i]); err != nil {
+			return measurements.ImageMeasurementsV2{}, err
+		}
+	}
+	return measurements.MergeImageMeasurementsV2(measuremnts...)
+}
---
+++ image/upload/internal/cmd/measurementsupload.go
@@ -0,0 +1,78 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/edgelesssys/constellation/v2/internal/logger"
+	"github.com/edgelesssys/constellation/v2/internal/osimage/measurementsuploader"
+	"github.com/spf13/cobra"
+)
+
+// newMeasurementsUploadCmd creates a new upload command.
+func newMeasurementsUploadCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "upload",
+		Short: "Uploads OS image measurements to S3",
+		Long:  "Uploads OS image measurements to S3.",
+		Args:  cobra.ExactArgs(0),
+		RunE:  runMeasurementsUpload,
+	}
+
+	cmd.SetOut(os.Stdout)
+
+	cmd.Flags().String("measurements", "", "Path to measurements file to upload")
+	cmd.Flags().String("signature", "", "Path to signature file to upload")
+	cmd.Flags().String("region", "eu-central-1", "AWS region of the archive S3 bucket")
+	cmd.Flags().String("bucket", "cdn-constellation-backend", "S3 bucket name of the archive")
+	cmd.Flags().Bool("verbose", false, "Enable verbose output")
+
+	must(cmd.MarkFlagRequired("measurements"))
+	must(cmd.MarkFlagRequired("signature"))
+
+	return cmd
+}
+
+func runMeasurementsUpload(cmd *cobra.Command, _ []string) error {
+	workdir := os.Getenv("BUILD_WORKING_DIRECTORY")
+	if len(workdir) > 0 {
+		must(os.Chdir(workdir))
+	}
+
+	flags, err := parseUploadMeasurementsFlags(cmd)
+	if err != nil {
+		return err
+	}
+
+	log := logger.New(logger.PlainLog, flags.logLevel)
+	log.Debugf("Parsed flags: %+v", flags)
+
+	uploadC, err := measurementsuploader.New(cmd.Context(), flags.region, flags.bucket, log)
+	if err != nil {
+		return fmt.Errorf("uploading image info: %w", err)
+	}
+
+	measurements, err := os.Open(flags.measurementsPath)
+	if err != nil {
+		return fmt.Errorf("uploading image measurements: opening measurements file: %w", err)
+	}
+	defer measurements.Close()
+	signature, err := os.Open(flags.signaturePath)
+	if err != nil {
+		return fmt.Errorf("uploading image measurements: opening signature file: %w", err)
+	}
+	defer signature.Close()
+
+	measurementsURL, signatureURL, err := uploadC.Upload(cmd.Context(), measurements, signature)
+	if err != nil {
+		return fmt.Errorf("uploading image info: %w", err)
+	}
+	log.Infof("Uploaded image measurements to %s (and signature to %s)", measurementsURL, signatureURL)
+	return nil
+}
