a8f3968e57ac9a5b915bf4e80556c0fd5227dc67
Malte Poll <1780588+malt3@users.noreply.github.com>
2023-06-15 13:20:30 +0200

    nix: add python toolchain deps

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@
     pkgs.glibc
     pkgs.gcc
     pkgs.jdk11 # TODO(katexochen): investigate why our build chain doesn't work on NixOS
+    pkgs.libxcrypt-legacy # TODO(malt3): python toolchain depends on this. Remove once https://github.com/bazelbuild/rules_python/issues/1211 is resolved
   ];
 })
 .env
