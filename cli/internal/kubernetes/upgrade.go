f59c1fe97cbc23166c099ec90056e430eea3500a
Adrian Stobbe <stobbe.adrian@gmail.com>
2023-07-07 11:17:20 +0200

    fix comments

--- cli/internal/kubernetes/upgrade.go
+++ cli/internal/kubernetes/upgrade.go
@@ -503,20 +503,22 @@ type stableClient struct {
 	client kubernetes.Interface
 }
 
-// getCurrent returns a ConfigMap given it's name.
+// GetCurrentConfigMap returns a ConfigMap given it's name.
 func (u *stableClient) GetCurrentConfigMap(ctx context.Context, name string) (*corev1.ConfigMap, error) {
 	return u.client.CoreV1().ConfigMaps(constants.ConstellationNamespace).Get(ctx, name, metav1.GetOptions{})
 }
 
-// update updates the given ConfigMap.
+// UpdateConfigMap updates the given ConfigMap.
 func (u *stableClient) UpdateConfigMap(ctx context.Context, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error) {
 	return u.client.CoreV1().ConfigMaps(constants.ConstellationNamespace).Update(ctx, configMap, metav1.UpdateOptions{})
 }
 
+// CreateConfigMap creates the given ConfigMap.
 func (u *stableClient) CreateConfigMap(ctx context.Context, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error) {
 	return u.client.CoreV1().ConfigMaps(constants.ConstellationNamespace).Create(ctx, configMap, metav1.CreateOptions{})
 }
 
+// KubernetesVersion returns the Kubernetes version of the cluster.
 func (u *stableClient) KubernetesVersion() (string, error) {
 	serverVersion, err := u.client.Discovery().ServerVersion()
 	if err != nil {
