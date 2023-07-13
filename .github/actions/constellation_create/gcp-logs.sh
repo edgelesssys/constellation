43076e96a6074395c3e9f0556f074a1abd4e37dc
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-07-13 16:28:33 +0200

    ci: fix resource selection for serial log downloading (#2101)
    Co-authored-by: Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
    Co-authored-by: Otto Bittner <cobittner@posteo.net>

--- .github/actions/constellation_create/gcp-logs.sh
+++ .github/actions/constellation_create/gcp-logs.sh
@@ -3,49 +3,19 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-pushd constellation-terraform
-controlInstanceGroup=$(
-  terraform show -json |
-    jq -r .'values.root_module.child_modules[] |
-    select(.address == "module.instance_group_control_plane") |
-    .resources[0].values.base_instance_name'
-)
-workerInstanceGroup=$(
-  terraform show -json |
-    jq -r .'values.root_module.child_modules[] |
-    select(.address == "module.instance_group_worker") |
-     .resources[0].values.base_instance_name'
-)
-zone=$(
-  terraform show -json |
-    jq -r .'values.root_module.child_modules[] |
-    select(.address == "module.instance_group_control_plane") |
-     .resources[0].values.zone'
-)
-popd
+echo "Using Zone: ${1}"
+echo "Using Constellation UID: ${2}"
 
-controlInstances=$(
-  gcloud compute instance-groups managed list-instances "${controlInstanceGroup##*/}" \
-    --zone "${zone}" \
-    --format=json |
-    jq -r '.[] | .instance'
+allInstances=$(
+  gcloud compute instances list \
+    --filter="labels.constellation-uid=${2}" \
+    --format=json | yq eval '.[] | .name' -
 )
-workerInstances=$(
-  gcloud compute instance-groups managed list-instances "${workerInstanceGroup##*/}" \
-    --zone "${zone}" \
-    --format=json |
-    jq -r '.[] | .instance'
-)
-
-allInstances="${controlInstances} ${workerInstances}"
-
-printf "Fetching logs for %s and %s\n" "${controlInstances}" "${workerInstances}"
 
 for instance in ${allInstances}; do
-  shortName=${instance##*/}
-  printf "Fetching for %s\n" "${shortName}"
+  printf "Fetching for %s\n" "${instance}"
   gcloud compute instances get-serial-port-output "${instance}" \
     --port 1 \
     --start 0 \
-    --zone "${zone}" > "${shortName}".log
+    --zone "${1}" > "${instance}".log
 done
