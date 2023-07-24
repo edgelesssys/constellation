18e752163b3e1ffe8edc2a5f62416a179915024d
Adrian Stobbe <stobbe.adrian@gmail.com>
2023-07-24 13:52:24 +0200

    only add awsLB to versions if installed

--- cli/internal/helm/client.go
+++ cli/internal/helm/client.go
@@ -210,13 +210,16 @@ func (c *Client) Versions() (ServiceVersions, error) {
 		return ServiceVersions{}, fmt.Errorf("getting %s version: %w", awsLBControllerInfo.releaseName, err)
 	}
 
-	return ServiceVersions{
+	res := ServiceVersions{
 		cilium:                 compatibility.EnsurePrefixV(ciliumVersion),
 		certManager:            compatibility.EnsurePrefixV(certManagerVersion),
 		constellationOperators: compatibility.EnsurePrefixV(operatorsVersion),
 		constellationServices:  compatibility.EnsurePrefixV(servicesVersion),
-		awsLBController:        compatibility.EnsurePrefixV(awsLBVersion),
-	}, nil
+	}
+	if awsLBVersion != "" {
+		res.awsLBController = compatibility.EnsurePrefixV(awsLBVersion)
+	}
+	return res, nil
 }
 
 // currentVersion returns the version of the currently installed helm release.
