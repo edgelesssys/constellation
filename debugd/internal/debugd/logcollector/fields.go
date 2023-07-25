e66ce950bf8ae2b496e6b944d338146f9436c5b0
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-07-25 08:27:11 +0200

    update fields

--- debugd/internal/debugd/logcollector/fields.go
+++ debugd/internal/debugd/logcollector/fields.go
@@ -14,12 +14,19 @@ func InfoFields() (string, map[string]struct{}) {
 		"is_debug_cluster": {}, // whether the cluster is a debug cluster
 
 		// GitHub workflow information, see https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-		"github.actor":       {},
-		"github.workflow":    {},
-		"github.run-id":      {},
-		"github.run-attempt": {},
-		"github.ref-name":    {},
-		"github.sha":         {},
-		"github.runner-os":   {},
+		"github.actor":            {},
+		"github.workflow":         {},
+		"github.run-id":           {},
+		"github.run-attempt":      {},
+		"github.ref-name":         {},
+		"github.sha":              {},
+		"github.runner-os":        {},
+		"github.e2e-test-payload": {},
+		"github.is-debug-cluster": {},
+		// cloud provider used in e2e test. If deployed with debugd, this is a duplicate as its also
+		// available in the metadata. If deployed through K8s in e2e tests with a stable image, this
+		// is where the cloud provider is saved in.
+		"github.e2e-test-provider": {},
+		"deployment-type":          {}, // deployment type, e.g. "debugd", "k8s"
 	}
 }
