04dc6256e644b63b60d9b38f38346786082770a2
Adrian Stobbe <stobbe.adrian@gmail.com>
2023-07-25 10:54:47 +0200

    cli: only install aws-lb and csi charts conditionally (#2131)
    * init
    
    * upgrade csi chart conditionally

--- cli/internal/helm/client.go
+++ cli/internal/helm/client.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/edgelesssys/constellation/v2/cli/internal/clusterid"
+	"github.com/edgelesssys/constellation/v2/internal/cloud/cloudprovider"
 	"github.com/edgelesssys/constellation/v2/internal/compatibility"
 	"github.com/edgelesssys/constellation/v2/internal/config"
 	"github.com/edgelesssys/constellation/v2/internal/constants"
@@ -109,8 +110,7 @@ func (c *Client) Upgrade(ctx context.Context, config *config.Config, idFile clus
 	upgradeErrs := []error{}
 	upgradeReleases := []*chart.Chart{}
 	newReleases := []*chart.Chart{}
-
-	for _, info := range []chartInfo{ciliumInfo, certManagerInfo, constellationOperatorsInfo, constellationServicesInfo, csiInfo, awsLBControllerInfo} {
+	for _, info := range getManagedCharts(config) {
 		c.log.Debugf("Checking release %s", info.releaseName)
 		chart, err := loadChartsDir(helmFS, info.path)
 		if err != nil {
@@ -185,6 +185,17 @@ func (c *Client) Upgrade(ctx context.Context, config *config.Config, idFile clus
 	}
 
 	return errors.Join(upgradeErrs...)
+}
+
+func getManagedCharts(config *config.Config) []chartInfo {
+	charts := []chartInfo{ciliumInfo, certManagerInfo, constellationOperatorsInfo, constellationServicesInfo, csiInfo}
+	if config.GetProvider() == cloudprovider.AWS {
+		charts = append(charts, awsLBControllerInfo)
+	}
+	if config.DeployCSIDriver() {
+		charts = append(charts, csiInfo)
+	}
+	return charts
 }
 
 // Versions queries the cluster for running versions and returns a map of releaseName -> version.
