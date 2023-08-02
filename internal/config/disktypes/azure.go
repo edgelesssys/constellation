ba5e5e145cb1a7561575fae433da0bb6639d7a11
Malte Poll <mp@edgeless.systems>
2023-08-02 10:23:51 +0200

    config: define lists of valid disk types

---
+++ internal/config/disktypes/azure.go
@@ -0,0 +1,16 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package disktypes
+
+// AzureDiskTypes are valid Azure disk types.
+var AzureDiskTypes = []string{
+	"Premium_LRS",
+	"Premium_ZRS",
+	"Standard_LRS",
+	"StandardSSD_LRS",
+	"StandardSSD_ZRS",
+}
