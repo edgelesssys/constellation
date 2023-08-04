d92cf2e7f3c359e9d469dce9a5e2ca8201e13fed
Malte Poll <mp@edgeless.systems>
2023-08-04 16:42:18 +0200

    cdbg: implement workspace / "-C" flag and "--bindir"

--- debugd/internal/cdbg/cmd/root.go
+++ debugd/internal/cdbg/cmd/root.go
@@ -8,9 +8,9 @@ SPDX-License-Identifier: AGPL-3.0-only
 package cmd
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/edgelesssys/constellation/v2/internal/constants"
 	"github.com/spf13/cobra"
 )
 
@@ -20,9 +20,12 @@ func newRootCmd() *cobra.Command {
 		Short: "Constellation debugging client",
 		Long: `cdbg is the constellation debugging client.
 	It connects to Constellation instances running debugd and deploys a self-compiled version of the bootstrapper.`,
+		PersistentPreRunE: preRunRoot,
 	}
-	cmd.PersistentFlags().String("config", constants.ConfigFilename, "Constellation config file")
+	cmd.PersistentFlags().StringP("workspace", "C", "", "path to the Constellation workspace")
 	cmd.PersistentFlags().Bool("force", false, "disables version validation errors - might result in corrupted clusters")
+
+	must(cmd.MarkPersistentFlagDirname("workspace"))
 
 	cmd.AddCommand(newDeployCmd())
 	return cmd
@@ -34,4 +37,29 @@ func Execute() {
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func preRunRoot(cmd *cobra.Command, _ []string) error {
+	cmd.SilenceUsage = true
+
+	workspace, err := cmd.Flags().GetString("workspace")
+	if err != nil {
+		return fmt.Errorf("getting workspace flag: %w", err)
+	}
+
+	// Change to workspace directory if set.
+	if workspace != "" {
+		if err := os.Chdir(workspace); err != nil {
+			return fmt.Errorf("changing from current directory to workspace %q: %w", workspace, err)
+		}
+	}
+
+	return nil
+}
+
+func must(err error) {
+	if err == nil {
+		return
+	}
+	panic(err)
 }
