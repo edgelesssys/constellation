9434f0c57cd976873d4141220011297ad9a80d4c
Malte Poll <mp@edgeless.systems>
2023-09-28 08:57:16 +0000

    wip

--- nix/shells/default.nix
+++ nix/shells/default.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
+
+(pkgs.buildFHSUserEnv {
+  name = "bazel-userenv";
+  targetPkgs = with pkgs; pkgs: [
     bazel_6
+    glibc
     git
   ];
-}
+  #extraBwrapArgs = [
+  #  "--bind-try ~/.cache ~/.cache"
+  #];
+}).env
