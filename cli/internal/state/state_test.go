d8761bda67c8d6a081d4dba3572a24c1a8448a46
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-10-17 08:29:29 +0200

    add missing dep
    Signed-off-by: Moritz Sanft <58110325+msanft@users.noreply.github.com>

--- cli/internal/state/state_test.go
+++ cli/internal/state/state_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/siderolabs/talos/pkg/machinery/config/encoder"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 var defaultState = &State{
