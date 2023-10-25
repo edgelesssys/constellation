cae65df87ca3fb88b852b14bbf60e5e5c634266b
miampf <miampf@proton.me>
2023-10-25 13:16:06 +0200

    removed Sync() from debugLog since slog doesn't need flushing

--- cli/internal/cmd/log.go
+++ cli/internal/cmd/log.go
@@ -7,24 +7,24 @@ SPDX-License-Identifier: AGPL-3.0-only
 package cmd
 
 import (
+	"log/slog"
+
 	"github.com/edgelesssys/constellation/v2/internal/logger"
 	"github.com/spf13/cobra"
-	"go.uber.org/zap/zapcore"
 )
 
 type debugLog interface {
 	Debugf(format string, args ...any)
-	Sync()
 }
 
 func newCLILogger(cmd *cobra.Command) (debugLog, error) {
-	logLvl := zapcore.InfoLevel
+	logLvl := slog.LevelInfo
 	debugLog, err := cmd.Flags().GetBool("debug")
 	if err != nil {
 		return nil, err
 	}
 	if debugLog {
-		logLvl = zapcore.DebugLevel
+		logLvl = slog.LevelDebug
 	}
 
 	return logger.New(logger.PlainLog, logLvl), nil
