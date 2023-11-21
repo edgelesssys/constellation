cb96a768d03272a635da00ed85e4f5bf89ba3727
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-11-21 17:28:22 +0100

    terraform-provider: init
    Signed-off-by: Moritz Sanft <58110325+msanft@users.noreply.github.com>

---
+++ terraform-provider/README.md
@@ -0,0 +1,7 @@
+# Constellation Terraform provider
+
+## Use the local provider
+
+```bash
+TF_CLI_CONFIG_FILE=config.tfrc terraform <command>
+```
---
+++ terraform-provider/main.go
@@ -0,0 +1,37 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
+)
+
+// TODO(msanft): Set this accordingly in the release CI.
+var version string = "dev"
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		// TODO(msanft): Verify that this will be the published name.
+		Address: "registry.terraform.io/edgelesssys/constellation",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
