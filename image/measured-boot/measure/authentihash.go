68933f72069116a205cbd88a364486594568aefe
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-01-29 16:36:23 +0100

    deps: update go-uefi and use new authenticode package

--- image/measured-boot/measure/authentihash.go
+++ image/measured-boot/measure/authentihash.go
@@ -8,24 +8,35 @@ package measure
 
 import (
 	"bytes"
+	"crypto"
 	"fmt"
-	"hash"
 	"io"
 
-	"github.com/foxboron/go-uefi/efi/pecoff"
+	"github.com/foxboron/go-uefi/authenticode"
 )
 
 // Authentihash returns the PE/COFF hash / Authentihash of a file.
-func Authentihash(r io.Reader, h hash.Hash) ([]byte, error) {
+func Authentihash(r io.Reader, h crypto.Hash) ([]byte, error) {
+	readerAt, err := getReaderAt(r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get readerAt: %v", err)
+	}
+
+	bin, err := authenticode.Parse(readerAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pe file: %v", err)
+	}
+	return bin.Hash(h), nil
+}
+
+func getReaderAt(r io.Reader) (io.ReaderAt, error) {
+	if ra, ok := r.(io.ReaderAt); ok {
+		return ra, nil
+	}
+
 	buf := new(bytes.Buffer)
 	if _, err := buf.ReadFrom(r); err != nil {
 		return nil, fmt.Errorf("failed to read pe file: %v", err)
 	}
-
-	signingCtx := pecoff.PECOFFChecksum(buf.Bytes())
-	pecoff.PaddSigCtx(signingCtx)
-
-	h.Write(signingCtx.SigData.Bytes())
-
-	return h.Sum(nil), nil
+	return bytes.NewReader(buf.Bytes()), nil
 }
