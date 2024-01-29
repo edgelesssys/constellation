7ce5caae248e7b5002f2a6581fadb90e7c5a1c44
Adrian Stobbe <stobbe.adrian@gmail.com>
2024-01-29 15:21:30 +0100

    terraform-provider: only print license warning once in provider (#2865)
    Co-authored-by: Moritz Sanft <58110325+msanft@users.noreply.github.com>

--- terraform-provider-constellation/internal/provider/cluster_resource.go
+++ terraform-provider-constellation/internal/provider/cluster_resource.go
@@ -449,8 +449,8 @@ func (r *ClusterResource) ModifyPlan(ctx context.Context, req resource.ModifyPla
 
 	licenseID := plannedState.LicenseID.ValueString()
 	if licenseID == "" {
-		resp.Diagnostics.AddWarning("Constellation license not found.",
-			"Using community license.\nFor details, see https://docs.edgeless.systems/constellation/overview/license")
+		resp.Diagnostics.AddWarning("Constellation license ID not set.",
+			"Continuing with community license.")
 	}
 	if licenseID == license.CommunityLicense {
 		resp.Diagnostics.AddWarning("Using community license.",
