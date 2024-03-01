3d7b8c3596b3426753256be71fc15d7bb3cda06e
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-03-01 16:05:08 +0100

    cli: add STACKIT to constellation config instance-types

---
+++ internal/config/instancetypes/stackit.go
@@ -0,0 +1,16 @@
+/*
+Copyright (c) Edgeless Systems GmbH
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
+package instancetypes
+
+// STACKITInstanceTypes are valid STACKIT instance types.
+var STACKITInstanceTypes = []string{
+	"m1a.2cd",
+	"m1a.4cd",
+	"m1a.8cd",
+	"m1a.16cd",
+	"m1a.30cd",
+}
