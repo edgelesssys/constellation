54e3f3826d43a47d692e74812e2f5932ef86e9cd
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-03-25 11:43:08 +0100

    wip: separate gcp packages

--- internal/attestation/gcp/restclient.go
+++ internal/attestation/gcp/restclient.go
@@ -1,9 +1,3 @@
-/*
-Copyright (c) Edgeless Systems GmbH
-
-SPDX-License-Identifier: AGPL-3.0-only
-*/
-
 package gcp
 
 import (
@@ -16,39 +10,11 @@ import (
 
 	compute "cloud.google.com/go/compute/apiv1"
 	"cloud.google.com/go/compute/apiv1/computepb"
-	"github.com/edgelesssys/constellation/v2/internal/attestation"
-	"github.com/edgelesssys/constellation/v2/internal/attestation/variant"
 	"github.com/edgelesssys/constellation/v2/internal/attestation/vtpm"
-	"github.com/edgelesssys/constellation/v2/internal/config"
 	"github.com/google/go-tpm-tools/proto/attest"
-	"github.com/googleapis/gax-go/v2"
+	"github.com/googleapis/gax-go"
 	"google.golang.org/api/option"
 )
-
-const minimumGceVersion = 1
-
-// Validator for GCP confidential VM attestation.
-type Validator struct {
-	variant.GCPSEVES
-	*vtpm.Validator
-
-	restClient func(context.Context, ...option.ClientOption) (gcpRestClient, error)
-}
-
-// NewValidator initializes a new GCP validator with the provided PCR values.
-func NewValidator(cfg *config.GCPSEVES, log attestation.Logger) *Validator {
-	v := &Validator{
-		restClient: newInstanceClient,
-	}
-	v.Validator = vtpm.NewValidator(
-		cfg.Measurements,
-		v.trustedKeyFromGCEAPI,
-		validateCVM,
-		log,
-	)
-
-	return v
-}
 
 type gcpRestClient interface {
 	GetShieldedInstanceIdentity(ctx context.Context, req *computepb.GetShieldedInstanceIdentityInstanceRequest, opts ...gax.CallOption) (*computepb.ShieldedInstanceIdentity, error)
@@ -101,20 +67,4 @@ func (v *Validator) trustedKeyFromGCEAPI(ctx context.Context, attDoc vtpm.Attest
 	}
 
 	return x509.ParsePKIXPublicKey(block.Bytes)
-}
-
-// validateCVM checks that the machine state represents a GCE AMD-SEV VM.
-func validateCVM(_ vtpm.AttestationDocument, state *attest.MachineState) error {
-	gceVersion := state.Platform.GetGceVersion()
-	if gceVersion < minimumGceVersion {
-		return fmt.Errorf("outdated GCE version: %v (require >= %v)", gceVersion, minimumGceVersion)
-	}
-
-	tech := state.Platform.Technology
-	wantTech := attest.GCEConfidentialTechnology_AMD_SEV
-	if tech != wantTech {
-		return fmt.Errorf("unexpected confidential technology: %v (expected: %v)", tech, wantTech)
-	}
-
-	return nil
 }
