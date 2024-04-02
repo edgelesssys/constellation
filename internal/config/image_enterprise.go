b72f0eb6e0a5818dbde1294eb16193a953afb974
katexochen <49727155+katexochen@users.noreply.github.com>
2024-04-02 21:56:33 +0000

    image: update measurements and image version

--- internal/config/image_enterprise.go
+++ internal/config/image_enterprise.go
@@ -10,5 +10,5 @@ package config
 
 const (
 	// defaultImage is the default image to use.
-	defaultImage = "ref/main/stream/nightly/v2.17.0-pre.0.20240325133609-febe8f0801a3"
+	defaultImage = "ref/main/stream/nightly/v2.17.0-pre.0.20240402172122-d8d2cd48c139"
 )
