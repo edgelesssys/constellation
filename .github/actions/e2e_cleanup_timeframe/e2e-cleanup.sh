bc3cfe1145a5e0c74fcd3bd70fa5f398e300a42c
miampf <miampf@proton.me>
2024-04-25 12:45:44 +0200

    removed testcase

--- .github/actions/e2e_cleanup_timeframe/e2e-cleanup.sh
+++ .github/actions/e2e_cleanup_timeframe/e2e-cleanup.sh
@@ -63,8 +63,6 @@ done
 mapfile -t database_ids < <(echo "${database_ids[@]}")
 mapfile -td " " database_ids < <(echo "${database_ids[@]}")
 
-database_ids=("8750778451")
-
 echo "[*] downloading terraform state artifacts"
 for id in "${database_ids[@]}"; do
   if [[ $id == *[^[:space:]]* ]]; then
