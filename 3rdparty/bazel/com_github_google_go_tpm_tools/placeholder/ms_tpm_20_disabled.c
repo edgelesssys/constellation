d960121cba549468af0f965fd22817548af9643d
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-05-21 17:56:37 +0200

    bazel: update BUILD files for rules_go bzlmod migration

--- 3rdparty/bazel/com_github_google_go_tpm_tools/placeholder/ms_tpm_20_disabled.c
+++
@@ -1,9 +0,0 @@
-#include "Platform.h"
-#include "Tpm.h"
-
-int g_inFailureMode = 0;
-
-void _plat__Reset(bool forceManufacture) {}
-
-void _plat__RunCommand(uint32_t requestSize, unsigned char *request,
-                       uint32_t *responseSize, unsigned char **response) {}
