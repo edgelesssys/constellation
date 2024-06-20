0368047939b37e3139e1b82d4982417095e74b5b
Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
2024-06-20 08:41:46 +0200

    helm: update Azure CSI, GCP CSI, and CSI snapshotter charts (#3175)
    * Update GCP CSI chart to v1.4.0
    * Update Azure CSI chart to v1.4.0
    * Update CSI snapshotter from v6.2.2 to v8.0.1
    
    ---------
    
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- internal/constellation/helm/update-csi-charts.sh
+++ internal/constellation/helm/update-csi-charts.sh
@@ -71,10 +71,10 @@ download_chart() {
 download_chart "https://github.com/edgelesssys/constellation-aws-ebs-csi-driver" "v1.2.0" "charts/aws-ebs-csi-driver" "aws-csi-driver"
 
 ## Azure CSI Driver
-download_chart "https://github.com/edgelesssys/constellation-azuredisk-csi-driver" "v1.3.0" "charts/edgeless" "azuredisk-csi-driver"
+download_chart "https://github.com/edgelesssys/constellation-azuredisk-csi-driver" "v1.4.0" "charts/edgeless" "azuredisk-csi-driver"
 
 ## GCP CSI Driver
-download_chart "https://github.com/edgelesssys/constellation-gcp-compute-persistent-disk-csi-driver" "v1.3.0" "charts" "gcp-compute-persistent-disk-csi-driver"
+download_chart "https://github.com/edgelesssys/constellation-gcp-compute-persistent-disk-csi-driver" "v1.4.0" "charts" "gcp-compute-persistent-disk-csi-driver"
 
 ## OpenStack CSI Driver (cinder)
 download_chart "https://github.com/edgelesssys/constellation-cloud-provider-openstack" "v1.0.2" "charts/cinder-csi-plugin" "openstack-cinder-csi"
