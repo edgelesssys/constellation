8ef5ea2efe48f44bc5cbdacc5bcd15511cc7440a
Markus Rudy <mr@edgeless.systems>
2024-09-13 09:47:33 +0200

    helm: fix kubeadm bugs caused by CoreDNS installation (#3353)
    * helm: rename CoreDNS configmap
    
    * upgrade-agent: ignore CoreDNS preflight errors
    
    * fixup! helm: rename CoreDNS configmap

--- cli/internal/cmd/apply_test.go
+++ cli/internal/cmd/apply_test.go
@@ -554,6 +554,7 @@ func (s *stubConstellApplier) Init(context.Context, atls.Validator, *state.State
 
 type helmApplier interface {
 	AnnotateCoreDNSResources(context.Context) error
+	CleanupCoreDNSResources(ctx context.Context) error
 	PrepareHelmCharts(
 		flags helm.Options, stateFile *state.State, serviceAccURI string, masterSecret uri.MasterSecret,
 	) (
