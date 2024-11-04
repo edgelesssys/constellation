930794ca4748310b87208ddb36dbbd987afbd1c7
edgelessci <edgelessci@users.noreply.github.com>
2024-11-04 08:10:26 +0000

    deps: update versions to v2.19.1

--- internal/config/image_enterprise.go
+++ internal/config/image_enterprise.go
@@ -10,5 +10,5 @@ package config
 
 const (
 	// defaultImage is the default image to use.
-	defaultImage = "ref/main/stream/nightly/v2.20.0-pre.0.20241029190319-7458d0e8921a"
+	defaultImage = "v2.19.1"
 )
