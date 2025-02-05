534492fda364a357e65b26779a9d618eeb765cca
miampf <miampf@proton.me>
2025-02-05 16:59:41 +0100

    fix: use correct path to cert in CLI

--- terraform/infrastructure/azure/main.tf
+++ terraform/infrastructure/azure/main.tf
@@ -307,7 +307,7 @@ Host ${azurerm_public_ip.loadbalancer_ip[0].fqdn}
 
 Host *
   PreferredAuthentications publickey
-  CertificateFile=ca_cert.pub
+  CertificateFile=constellation-terraform/ca_cert.pub
   User root
   ProxyJump ${azurerm_public_ip.loadbalancer_ip[0].fqdn}
 EOF
