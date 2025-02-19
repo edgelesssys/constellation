be965abc9a3be24a49c06d7084cf27f23eb1bbe5
Leonard Cohnen <lc@edgeless.systems>
2025-02-19 11:38:11 +0100

    debug: sleep for 30 days before rebooting

--- bootstrapper/internal/reboot/reboot_linux.go
+++ bootstrapper/internal/reboot/reboot_linux.go
@@ -23,7 +23,8 @@ func Reboot(e error) {
 	}
 	_ = syslogWriter.Err(e.Error())
 	_ = syslogWriter.Emerg("bootstrapper has encountered a non recoverable error. Rebooting...")
-	time.Sleep(time.Minute) // sleep to allow the message to be written to syslog and seen by the user
+	// Debug: sleep for 30 days
+	time.Sleep(30 * 24 * time.Hour) // sleep to allow the message to be written to syslog and seen by the user
 
 	_ = syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART)
 }
