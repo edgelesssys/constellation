07a955c73c5fec6586cca66a720ffda7bee7c4bb
miampf <miampf@proton.me>
2025-04-03 17:59:10 +0200

    cli: write known_hosts file

--- cli/internal/cmd/ssh.go
+++ cli/internal/cmd/ssh.go
@@ -74,7 +74,10 @@ func writeCertificateForKey(cmd *cobra.Command, keyPath string, fh file.Handler,
 		return fmt.Errorf("generating SSH emergency CA key: %s", err)
 	}
 
-	debugLogger.Debug("SSH CA KEY generated", "public-key", string(ssh.MarshalAuthorizedKey(ca.PublicKey())))
+	marshalledKey := string(ssh.MarshalAuthorizedKey(ca.PublicKey()))
+	debugLogger.Debug("SSH CA KEY generated", "public-key", marshalledKey)
+	knownHostsContent := fmt.Sprintf("@cert-authority * %s", marshalledKey)
+	fh.Write("./known_hosts", []byte(knownHostsContent), file.OptMkdirAll)
 
 	keyBuffer, err := fh.Read(keyPath)
 	if err != nil {
