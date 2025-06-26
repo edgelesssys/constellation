b8790fca6858e2033361c35235322c842c85b0ed
edgelessci <edgelessci@users.noreply.github.com>
2025-06-26 22:35:25 +0000

    image: update measurements and image version

--- internal/config/image_enterprise.go
+++ internal/config/image_enterprise.go
@@ -10,5 +10,5 @@ package config
 
 const (
 	// defaultImage is the default image to use.
-	defaultImage = "ref/main/stream/nightly/v2.24.0-pre.0.20250624103949-1216a2af72d0"
+	defaultImage = "ref/main/stream/nightly/v2.24.0-pre.0.20250625080937-dc9fca34a961"
 )
